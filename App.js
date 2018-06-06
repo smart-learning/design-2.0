781cc8fd91b31b817de0955f556541a512d58669
KyeJune Hong <hohoyaing@gmail.com>
2018-06-06 23:59:21 +0900

    사이드박 테스트용 버튼 추가 하면서 앱 전체 Floating객제 생성

--- App.js
+++ App.js
@@ -1,29 +1,56 @@
 import React from 'react';
-import {createDrawerNavigator} from "react-navigation";
+import {createDrawerNavigator, DrawerActions } from "react-navigation";
 import HomeScreen from './src/scripts/pages/home/HomeScreen';
 import VideoScreen from './src/scripts/pages/video/VideoScreen';
 import AudioScreen from './src/scripts/pages/audio/AudioScreen';
 import MyScreen from './src/scripts/pages/my/MyScreen';
 import Playground from "./src/scripts/pages/Playground";
+import {Button, View} from "react-native";
+import Store from "./src/js/store";
 
-export default createDrawerNavigator({
-    HomeScreen: {
-        screen: HomeScreen,
-    },
+class App extends React.Component {
 
-    VideoScreen: {
-        screen: VideoScreen,
-    },
 
-    AudioScreen: {
-        screen: AudioScreen,
-    },
-
-    MyScreen: {
-        screen: MyScreen,
-    },
-
-    Playground:{
-        screen: Playground,
+    render() {
+        return <View style={{flex:1}}>
+            <AppDrawer ref={ navigatorRef => { Store.drawer = navigatorRef  } }/>
+            <View style={{ position:'absolute', bottom:0, right:0 }}>
+                <Button title="Open Side"
+                    onPress={()=>{
+                        Store.drawer.dispatch( DrawerActions.toggleDrawer() )
+                    }}
+                />
+            </View>
+        </View>
     }
-});
+}
+
+const AppDrawer = createDrawerNavigator(
+    {
+        HomeScreen: {
+            screen: HomeScreen,
+        },
+
+        VideoScreen: {
+            screen: VideoScreen,
+        },
+
+        AudioScreen: {
+            screen: AudioScreen,
+        },
+
+        MyScreen: {
+            screen: MyScreen,
+        },
+
+        Playground: {
+            screen: Playground,
+        }
+    },
+
+    {
+        headerMode:'float'
+    }
+);
+
+export default App
--- src/scripts/pages/home/HomeScreen.js
+++ src/scripts/pages/home/HomeScreen.js
@@ -1,35 +1,41 @@
 import React from "react";
-import Styles from "../../../styles/common";
-import {Image, Text, View} from "react-native";
+import {Image} from "react-native";
 import {createStackNavigator} from "react-navigation";
 import HomeSubScreen1 from "./HomeSubScreen1";
 import HomeSubScreen2 from "./HomeSubScreen2";
+import Styles from "../../../styles/common";
 
 const HomeScreen = createStackNavigator({
 
-    HomeScreen1:{
-        screen: HomeSubScreen1,
-        navigationOptions: ({ navigation }) => ({
-            title: `서브페이지 스택 헤더1`,
-        }),
+        HomeScreen1: {
+            screen: HomeSubScreen1,
+            // navigationOptions: ({ navigation }) => ({
+            //     title: `서브페이지 스택 헤더1`,
+            // }),
+        },
+
+        HomeScreen2: {
+            screen: HomeSubScreen2,
+            // navigationOptions: ({ navigation }) => ({
+            //     title: `서브페이지 스택 헤더2`,
+            // }),
+        }
     },
 
-    HomeScreen2:{
-        screen: HomeSubScreen2,
-        navigationOptions: ({ navigation }) => ({
-            title: `서브페이지 스택 헤더2`,
-        }),
+    {
+        headerMode: 'none'
     }
-});
+);
 
-HomeScreen.navigationOptions = {
+HomeScreen.navigationOptions = ({navigation})=>({
+
     drawerLabel: '홈',
-    drawerIcon: ({ tintColor }) => (
+    drawerIcon: ({tintColor}) => (
         <Image
             source={require('../../../images/chats-icon.png')}
             style={[Styles.sidebarIcon, {tintColor: tintColor}]}
         />
-    ),
-};
+    )
+});
 
 export default HomeScreen;
