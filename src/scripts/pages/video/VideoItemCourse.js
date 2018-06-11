c9bbb1fa202c94101c10ed30fa865ce0bdf41483
nanuRuya <uru@grotesq.com>
2018-06-11 20:36:04 +0900

    비디오 목록 컴포넌트 중간 커밋

--- src/scripts/pages/video/VideoItemCourse.js
+++ src/scripts/pages/video/VideoItemCourse.js
@@ -4,41 +4,20 @@ import {
 	ImageBackground,
 	Text,
 	View,
-	StyleSheet
+	StyleSheet,
 } from "react-native";
-import {COLOR_PRAMARY} from "../../../styles/common";
-// import CommonStyles from "../../../styles/common";
+import {COLOR_PRIMARY} from "../../../styles/common";
 import IcComment from "../../../images/ic-comment-light.png"
 import IcPin from "../../../images/ic-pin-light.png";
 import IcShare from "../../../images/ic-share-light.png";
 import IcStar from "../../../images/ic-star-light.png";
 import IcStarActive from "../../../images/ic-star.png";
 import IcView from "../../../images/ic-view-light.png";
-// import Dummy from "../../../images/dummy-videocourse.png";
-
-
-export default class VideoItemCourse extends React.Component {
-	render() {
-		return <View style={ styles.itemContainer }>
-			{/*타이틀*/}
-			<Text style={ styles.title }>Title</Text>
-
-			<Text style={ styles.subTitle }>sub title</Text>
-			<View style={ styles.thumbnail }>
-				<Text>Thumbnail</Text>
-			</View>
-			{/*<ImageBackground source={ Dummy } style={ styles.thumbnail }>*/}
-				{/*<Text>Inside</Text>*/}
-			{/*</ImageBackground>*/}
-			<Image source={ IcComment }/>
-			<Image source={ IcPin }/>
-			<Image source={ IcShare }/>
-			<Image source={ IcStar }/>
-			<Image source={ IcStarActive }/>
-			<Image source={ IcView }/>
-		</View>
-	}
-}
+import Dummy from "../../../images/dummy-videocourse.png";
+import IcPlay from "../../../images/ic-play.png";
+import Store from "../../commons/store";
+import {DrawerActions} from "react-navigation";
+import CommonStyles from "../../../styles/common";
 
 const styles = StyleSheet.create({
 	itemContainer: {
@@ -53,28 +32,56 @@ const styles = StyleSheet.create({
 	},
 	title: {
 		fontSize: 22,
-		color: COLOR_PRAMARY,
+		color: COLOR_PRIMARY,
 	},
 	subTitle: {
 		fontSize: 16,
 		color: '#999999'
 	},
 	thumbnail: {
-		position: 'absolute',
-		top: 0,
-		bottom: 0,
-		left: 0,
-		right: 0,
-		width: '88%',
+		position: 'relative',
+		width: '100%',
+		paddingTop: '21%',
+		paddingBottom: '21%',
 		justifyContent: 'center',
 		alignItems: 'center',
 	},
-	bigblue: {
-		color: 'blue',
-		fontWeight: 'bold',
-		fontSize: 30,
+	courseCount: {
+		position: 'absolute',
+		bottom: '14%',
+		left: '11.5%',
+		fontSize: 16,
+		color: '#ffffff'
 	},
-	red: {
-		color: 'red',
+	play: {
+		position: 'absolute',
+		top: '75%',
+		right: 20,
+		width: 50,
+		height: 50,
 	},
 });
+
+export default class VideoItemCourse extends React.Component {
+
+	render() {
+		return <View style={styles.itemContainer}>
+			{/*타이틀*/}
+			<Text style={styles.title}>Title</Text>
+			{/*서브타이틀*/}
+			<Text style={styles.subTitle}>sub title</Text>
+			{/*썸네일*/}
+			<ImageBackground source={Dummy} style={styles.thumbnail}>
+				<Text style={styles.courseCount}>11개 강의</Text>
+				<Image source={ IcPlay } style={styles.play}/>
+			</ImageBackground>
+			<View>
+				<Image source={ IcView }/>
+				<Image source={ IcStar }/>
+				<Image source={ IcComment }/>
+				<Image source={ IcPin }/>
+				<Image source={ IcShare }/>
+			</View>
+		</View>
+	}
+}
