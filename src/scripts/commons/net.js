199685570aefb927108997a5e4c0e5b64163d608
nanuRuya <uru@grotesq.com>
2018-06-18 00:14:57 +0900

    실제 데이터 더미로 입력 테스트

--- src/scripts/commons/net.js
+++ src/scripts/commons/net.js
@@ -1,37 +1,102 @@
-
-
-
 export default {
 
-	getVideoCourseList(){
-		return new Promise( (resolve, reject)=>{
-			resolve([
-				{
-					key: '0',
-					title: '쓸데 없는 것을 비워 삶의 가치를 배로 만드는 직장인 미니멀라이프 10가지 실천방법',
-					subTitle: '심플라이프, 단순한 학교 탁진현 칼럼니스트',
-					thumbnail: '',
-					courseCount: '00',
-					viewCount: '000',
-					starCount: '0.0',
-					reviewCount: '0',
-				},
-				{
-					key: '1',
-					title: '실무에서 바로 적용 가능한 엑셀 프로그램 핵심 정리!',
-					subTitle: '혜윰 케이비 김철 대표',
-					thumbnail: '',
-					courseCount: '00',
-					viewCount: '000',
-					starCount: '0.0',
-					reviewCount: '0',
-				},
-			]);
+	getVideoCourseList() {
+		return new Promise((resolve, reject) => {
+			resolve({
+				category: {},
+				items: [
+					{
+						key: "0",
+						ccode: "007001",
+						clip_count: 5,
+						clip_ids: "6055,6056,6057,6058,6059",
+						headline: "강의 만족도 1위 취업 컨설턴트의 성공하는 커리어 관리 노하우",
+						hit_count: 56,
+						id: 404,
+						idx: null,
+						image_url: "http://ec2-52-78-197-242.ap-northeast-2.compute.amazonaws.com/contentsUpImage/",
+						images: {
+							big: "http://ec2-52-78-197-242.ap-northeast-2.compute.amazonaws.com/static/courses/v100018/v100018_big.jpg",
+							list: "http://ec2-52-78-197-242.ap-northeast-2.compute.amazonaws.com/contentsUpImage/20180608161418_1.jpg",
+							wide: "http://ec2-52-78-197-242.ap-northeast-2.compute.amazonaws.com/contentsUpImage/20180608161418.jpg"
+						},
+						is_exculsive: true,
+						is_featured: false,
+						is_new: true,
+						like_count: 0,
+						play_time: "01:12:56",
+						progress: {},
+						review_count: 0,
+						star_avg: 0,
+						teacher: {
+							headline: "유튜브 취업 관련 강의 조회수 1위",
+							id: 222,
+							images: {
+								default: "http://ec2-52-78-197-242.ap-northeast-2.compute.amazonaws.com/contentsUpImage/teacher/20180511152459.jpg",
+								profile: "http://ec2-52-78-197-242.ap-northeast-2.compute.amazonaws.com/static/teacher/t000233_profile.jpg"
+							},
+							memo: "10년 이상 경력의 취업 전문 컨설턴트!<br><br>\r\n신길자 취업 전문 컨설턴트는 취업분야 베스트셀러 <뽑히는 취업>, <뽑히는 자기소개서>, <뽑히는 면접>의 저자이자 유튜브 자소서 강의 조회수 1위, 취업 특강 만족도 1위를 달성한 취업 전문 멘토이다. <br><br>\r\n진로설계 및 취업전략, 비전설정, 자기계발 분야의 강의와 상담 프로그램을 진행하고 있으며 취업 커뮤니티 '언니의 취업가게' 운영자이자 코리아써치 경력개발연구소 소장으로 수많은 취준생들을 합격으로 이끌어주고 있다. KBS,  MBC, 한국경제TV등 대한민국 다수의 매체에 출연, 취업 정보 제공 및 취업 강의를 진행하였다. 신길자 취업 컨설턴트가 전하는 양질의 취업 노하우는 취업 준비로 힘들어하는 모든 취준생들의 길잡이가 되어줄 것이다.",
+							name: "신길자 취업 컨설턴트"
+						},
+						title: "직장인이라면 누구나 알아야 할 몸값 올리는 경력관리기술 ",
+						type: "video-course",
+						url: "http://ec2-52-78-197-242.ap-northeast-2.compute.amazonaws.com/video-serise-info.php?groupkey=404",
+						user_zzim: false,
+						zzim_count: 0
+					},
+					{
+						key: "1",
+						ccode: "003001",
+						clip_count: 10,
+						clip_ids: "4816,4817,4818,4819,4820,4821,4822,4823,4824,4825",
+						headline: "직장 여성들의 리얼한 고민을 다 모았다, 속이 뻥 뚫리는 직장여성 실전 생존 팁!",
+						hit_count: 20,
+						id: 324,
+						idx: null,
+						image_url: "http://ec2-52-78-197-242.ap-northeast-2.compute.amazonaws.com/contentsUpImage/",
+						images: {
+							big: "http://ec2-52-78-197-242.ap-northeast-2.compute.amazonaws.com/static/courses/v200130/v200130_big.jpg",
+							list: "http://ec2-52-78-197-242.ap-northeast-2.compute.amazonaws.com/contentsUpImage/20180530154817_1.jpg",
+							wide: "http://ec2-52-78-197-242.ap-northeast-2.compute.amazonaws.com/contentsUpImage/20180530154817.jpg"
+						},
+						is_exculsive: false,
+						is_featured: false,
+						is_new: false,
+						like_count: 0,
+						play_time: "02:32:34",
+						progress: {},
+						review_count: 0,
+						star_avg: 0,
+						teacher: {
+							headline: "여성 커리어 전문가, 여자 라이프 스쿨 ",
+							id: 233,
+							images: {
+								"default": "http://ec2-52-78-197-242.ap-northeast-2.compute.amazonaws.com/contentsUpImage/teacher/20180601102127.png",
+								"profile": "http://ec2-52-78-197-242.ap-northeast-2.compute.amazonaws.com/static/teacher/t000255_profile.jpg"
+							},
+							memo: "여성 경력개발 교육업체 여자라이프스쿨 대표<br />\r\n고려대학교 여학생 진로 담당 외래교수<br />\r\n경기도 일가정양립전문가 지원단<br />\r\n​한국코치협회 인증 코치, 교류분석상담사<br />\r\n직장여성, 경력단절 커리어 교육과정 개발 다수 참여<br /> \r\n미국무성 IVLP (International Visitor Leadership Program) 여성리더 선발 및 한양대 교육공학 박사<br />\r\n<br />\r\n​저서로는 <다시, 일이 그리워질 때>, <왜 그녀들은 회사에서 인정받는 걸까>, <벌거벗은 이력서>, <여자라이프사전, 서른라이프사전> 등이 있다.",
+							name: "이재은 대표"
+						},
+						title: "일하는 여자들의 고민있수다",
+						type: "video-course",
+						url: "http://ec2-52-78-197-242.ap-northeast-2.compute.amazonaws.com/video-serise-info.php?groupkey=324",
+						user_zzim: false,
+						zzim_count: 0
+					}
+				],
+				pagination: {
+					current_page: 1,
+					has_next: true,
+					next_page: 2,
+					per_page: 10,
+					total: 128
+				}
+			});
 		});
 	},
 
-	getVideoClipList(){
-		return new Promise( (resolve, reject)=>{
+	getVideoClipList() {
+		return new Promise((resolve, reject) => {
 			resolve([
 				{
 					key: '0',
@@ -55,8 +120,8 @@ export default {
 		});
 	},
 
-	getBookList(){
-		return new Promise( (resolve, reject)=>{
+	getBookList() {
+		return new Promise((resolve, reject) => {
 			resolve([
 				{
 					key: '0',
@@ -67,6 +132,7 @@ export default {
 					viewCount: '000',
 					heartCount: '00',
 					commentCount: '00',
+					itemColor: '#E1DFDF',
 				},
 				{
 					key: '1',
@@ -77,6 +143,7 @@ export default {
 					viewCount: '000',
 					heartCount: '00',
 					commentCount: '00',
+					itemColor: '#F4E69F',
 				},
 			]);
 		});
