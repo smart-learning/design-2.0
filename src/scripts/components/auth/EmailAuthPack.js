237631117ac8944129d2a6756bbda58bdcecfb03
nanuRuya <uru@grotesq.com>
2018-06-27 16:44:05 +0900

    로그인페이지 중간커밋

--- src/scripts/components/auth/EmailAuthPack.js
+++ src/scripts/components/auth/EmailAuthPack.js
@@ -1,5 +1,49 @@
-import React, {Component} from 'react';
-import {Text, TextInput, TouchableOpacity, View} from "react-native";
+import React, { Component } from 'react';
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
+import {COLOR_PRIMARY} from "../../../styles/common";
+
+const styles = StyleSheet.create( {
+	contentContainer: {
+		width: '100%',
+	},
+	inputWrap: {
+		backgroundColor: '#ffffff',
+	},
+	inputBr: {
+		width: '100%',
+		height: 1,
+		backgroundColor: '#d8d8d8',
+	},
+	input: {
+		width: '100%',
+		height: 40,
+	},
+	btnSubmit: {
+		width: '100%',
+		height: 48,
+		marginTop: 10,
+		marginBottom: 10,
+		backgroundColor: COLOR_PRIMARY,
+	},
+	textSubmit: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		fontSize: 16,
+		lineHeight: 48,
+		textAlign: 'center',
+		fontWeight: 'bold',
+		color: '#ffffff',
+	},
+	linkWrap: {
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+	},
+	btnLinkText: {
+		fontSize: 12,
+		color: COLOR_PRIMARY,
+	}
+} );
 
 class EmailAuthPack extends Component {
 
@@ -13,30 +57,39 @@ class EmailAuthPack extends Component {
 	}
 
 	render() {
-		return <View>
-			<TextInput
-				value={this.state.email}
-				onChangeText={ text =>
-					this.setState({email: text})
-				}/>
+		return <View style={ styles.contentContainer }>
+			<View borderRadius={4} style={ styles.inputWrap }>
+				<TextInput
+					style={ styles.input }
+					value={this.state.email}
+					onChangeText={text =>
+						this.setState( { email: text } )
+					}/>
+				<View style={ styles.inputBr }/>
+				<TextInput
+					style={ styles.input }
+					value={this.state.password}
+					onChangeText={text =>
+						this.setState( { password: text } )
+					}/>
+			</View>
 
-			<TextInput
-				value={this.state.password}
-				onChangeText={ text =>
-					this.setState({password: text})
-				}/>
-
-			<TouchableOpacity>
-				<Text>윌라 계정으로</Text>
+			<TouchableOpacity activeOpacity={0.9}
+							  borderRadius={4}
+							  style={ styles.btnSubmit }
+			>
+				<Text style={ styles.textSubmit }>윌라 계정으로</Text>
 			</TouchableOpacity>
 
-			<TouchableOpacity>
-				<Text>비밀번호 찾기</Text>
-			</TouchableOpacity>
+			<View style={ styles.linkWrap }>
+				<TouchableOpacity activeOpacity={0.9}>
+					<Text style={ styles.btnLinkText }>비밀번호 찾기</Text>
+				</TouchableOpacity>
 
-			<TouchableOpacity>
-				<Text>무료 계정만들기</Text>
-			</TouchableOpacity>
+				<TouchableOpacity activeOpacity={0.9}>
+					<Text style={ styles.btnLinkText }>무료 계정만들기</Text>
+				</TouchableOpacity>
+			</View>
 		</View>
 	}
 }
--- src/scripts/pages/auth/LoginPage.js
+++ src/scripts/pages/auth/LoginPage.js
@@ -1,58 +1,125 @@
 import React from 'react';
-import Styles from "../../../styles/common";
-import {SafeAreaView} from "react-navigation";
-import {AsyncStorage, Button, Text } from "react-native";
-import {LoginButton, AccessToken} from "react-native-fbsdk";
+import CommonStyles from "../../../styles/common";
+import { SafeAreaView } from "react-navigation";
+import {
+	AsyncStorage,
+	Button,
+	Text,
+	Image,
+	ImageBackground,
+	StyleSheet,
+	View
+} from "react-native";
+import { LoginButton, AccessToken } from "react-native-fbsdk";
 import KakaoLoginButton from "../../components/auth/KakaoLoginButton";
 import EmailAuthPack from "../../components/auth/EmailAuthPack";
+import logo from '../../../images/logo-en-primary.png';
+import bgLogin from '../../../images/bg-signup.jpg';
+import icKakao from '../../../images/ic-kakao.png';
+import icFb from '../../../images/ic-fb.png';
+
+const styles = StyleSheet.create( {
+	loginContainer: {
+		position: 'relative',
+	},
+	background: {
+		width: '100%',
+		height: '100%',
+		paddingTop: 50,
+	},
+	logoWrap: {
+		position: 'absolute',
+		top: 20,
+		zIndex: 9,
+		alignItems: 'center',
+		height: 50,
+	},
+	logo: {
+		width: 110,
+		height: 28,
+		marginTop: 20,
+	},
+	contentWrap: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		width: '80%',
+		marginLeft: '10%',
+	},
+	headline: {
+		paddingBottom: 60,
+		textAlign: 'center',
+		fontSize: 40,
+		fontWeight: 'bold',
+		color: '#ffffff',
+	},
+	bulletText: {
+		marginTop: 15,
+		marginBottom: 15,
+		fontSize: 14,
+		fontWeight: 'bold',
+		textAlign: 'center',
+		color: '#ffffff'
+	}
+} );
 
 
 class LoginPage extends React.Component {
 
 
-    static navigationOptions = {
-        drawerLabel: ()=>null
-    };
+	static navigationOptions = {
+		drawerLabel: () => null
+	};
 
-    login = async ()=>{
-        await AsyncStorage.setItem('userToken', 'temp-token');
-        this.props.navigation.navigate('MyScreen');
-    }
+	// login = async () => {
+	// 	await AsyncStorage.setItem( 'userToken', 'temp-token' );
+	// 	this.props.navigation.navigate( 'MyScreen' );
+	// };
 
-    render() {
-        return <SafeAreaView style={[ Styles.container, {backgroundColor: '#ff0000'}]}>
+	render() {
+		return <SafeAreaView style={[ CommonStyles.container, styles.loginContainer ]}>
+			<View style={styles.logoWrap}>
+				<Image source={logo} style={styles.logo}></Image>
+			</View>
+			<ImageBackground source={bgLogin}
+							 style={styles.background}
+			>
+				<View style={styles.contentWrap}>
+					<Text style={styles.headline}>LOGIN</Text>
+					{/*<Button*/}
+						{/*title="Login"*/}
+						{/*onPress={this.login}*/}
+					{/*/>*/}
 
-            <Text>LOGIN</Text>
-            <Button
-                title="Login"
-                onPress={ this.login }
-            />
 
-
-			<LoginButton
-				onLoginFinished={
-					(error, result) => {
-						if (error) {
-							alert("login has error: " + result.error);
-						} else if (result.isCancelled) {
-							alert("login is cancelled.");
-						} else {
-							AccessToken.getCurrentAccessToken().then(
-								(data) => {
-									alert(data.accessToken.toString())
+					<LoginButton
+						onLoginFinished={
+							( error, result ) => {
+								if ( error ) {
+									alert( "login has error: " + result.error );
+								} else if ( result.isCancelled ) {
+									alert( "login is cancelled." );
+								} else {
+									AccessToken.getCurrentAccessToken().then(
+										( data ) => {
+											alert( data.accessToken.toString() )
+										}
+									)
 								}
-							)
+							}
 						}
-					}
-				}
-				onLogoutFinished={() => alert("logout.")}/>
+						onLogoutFinished={() => alert( "logout." )}/>
 
-			<KakaoLoginButton/>
+					<KakaoLoginButton/>
 
-			<EmailAuthPack/>
+					<Text style={ styles.bulletText }>OR</Text>
 
-        </SafeAreaView>
-    }
+					<EmailAuthPack/>
+				</View>
+
+			</ImageBackground>
+		</SafeAreaView>
+	}
 }
 
 
