d31c65d778388566e70233260311182428eb763b
nanuRuya <uru@grotesq.com>
2018-07-04 16:03:51 +0900

    오디오북 컴포넌트 부트스트랩, 타입 추가

--- src/scripts/components/audio/Book.js
+++ src/scripts/components/audio/Book.js
@@ -4,63 +4,169 @@ import {
 	Text,
 	View,
 	StyleSheet,
+	TouchableOpacity,
 } from "react-native";
-import {DrawerActions} from "react-navigation";
+import { DrawerActions } from "react-navigation";
 import CommonStyles from "../../../styles/common";
 import IcComment from "../../../images/ic-comment-dark.png"
 import IcPin from "../../../images/ic-pin-grey.png";
 import IcShare from "../../../images/ic-share-grey.png";
 import IcHeart from "../../../images/ic-heart-pink.png";
 import IcView from "../../../images/ic-view-dark.png";
-import Dummy from "../../../images/dummy-audioBook.png";
 
-const styles = StyleSheet.create({
+const styles = StyleSheet.create( {
 	itemContainer: {
 		position: 'relative',
 	},
 	alignJustify: {
 		flex: 1,
 		flexDirection: 'row',
-		// alignItems: 'center',
-		// justifyContent: 'center',
 	},
 	itemInfo: {
 		width: '100%',
-		height: 135,
-		paddingTop: 30,
-		paddingBottom: 30,
-		paddingLeft: 30,
+		height: 115,
+		padding: 20,
 		backgroundColor: '#ffffff',
 	},
 	itemContent: {
-		height: 145,
-		paddingTop: 30,
-		paddingBottom: 30,
-		paddingLeft: 30,
+		height: 130,
+		padding: 20,
+	},
+	bookInfoWrap: {
+		width: '60%',
+	},
+	sequence: {
+		width: 24,
+		fontSize: 16,
+		color: '#FF4F72',
 	},
 	bookThumbnail: {
 		position: 'absolute',
-		top: '17%',
-		right: 10,
-		width: 160,
-		height: 188,
+		top: 50,
+		right: 15,
+		width: 120,
+		height: 141,
 	},
 	bookThumbnailSize: {
 		width: '100%',
 		height: '100%',
-	}
-});
+	},
+	bookTitleBest: {
+		minHeight: 20,
+		maxHeight: 40,
+		fontSize: 16,
+		lineHeight: 20,
+		color: '#4a4a4a',
+	},
+	bookAuthorBest: {
+		fontSize: 12,
+		lineHeight: 20,
+		color: '#999999',
+	},
+	bookTitleRecommend: {
+		fontSize: 18,
+		color: '#000000',
+	},
+	bookAuthorRecommend: {
+		fontSize: 12,
+		color: '#4A4A4A',
+	},
+	bookMemoBest: {
+		width: '60%',
+		height: 60,
+		fontSize: 13,
+		color: '#5A5A5A',
+		lineHeight: 20,
+	},
+	bookMemoRecommend: {
+		width: '60%',
+		height: 60,
+		fontSize: 15,
+		color: '#ffffff',
+		lineHeight: 20,
+	},
+	socialButtonWrap: {
+		position: 'absolute',
+		top: 20,
+		right: 20,
+		zIndex: 9,
+	},
+	socialButton: {
+		width: 20,
+		height: 20,
+		marginLeft: 5,
+	},
+	playTime: {
+		fontSize: 12,
+		color: '#4A4A4A',
+	},
+	price: {
+		fontSize: 12,
+		color: '#FF4F72',
+	},
+	bar: {
+		width: 1,
+		height: 12,
+		marginLeft: 5,
+		marginRight: 5,
+		backgroundColor: '#C6C6C6',
+	},
+} );
 
 export default class Book extends React.Component {
+	constructor( props ) {
+		super( props );
+
+		this.state = {
+			//
+		};
+	}
+
 	render() {
-		return <View style={ styles.itemContainer }>
-			<View style={ [ styles.alignJustify, styles.itemInfo ] }>
-				<Text>1</Text>
-				<Text>2</Text>
+		return <View style={styles.itemContainer}>
+			{this.props.type === 'best' &&
+			<View style={[ styles.alignJustify, styles.socialButtonWrap ]}>
+				<TouchableOpacity activeOpacity={0.9}>
+					<Image source={IcPin} style={styles.socialButton}/>
+				</TouchableOpacity>
+				<TouchableOpacity activeOpacity={0.9}>
+					<Image source={IcShare} style={styles.socialButton}/>
+				</TouchableOpacity>
 			</View>
-			<View style={ [ {backgroundColor: this.props.itemColor}, styles.itemContent ] }><Text>2</Text></View>
+			}
+			<View style={[ styles.alignJustify, styles.itemInfo ]}>
+				{this.props.type === 'best' &&
+				<Text style={styles.sequence}>00</Text>
+				}
+				{this.props.type === 'best' &&
+				<View style={styles.bookInfoWrap}>
+					<Text style={styles.bookTitleBest}>{this.props.title}</Text>
+					<Text style={styles.bookAuthorBest}>{this.props.teacherName}</Text>
+					<View style={styles.alignJustify}>
+						<Text style={styles.playTime}>06시간 23분</Text>
+						<View style={styles.bar}/>
+						<Text style={styles.price}>₩11,900</Text>
+					</View>
+				</View>
+				}
+				{this.props.type === 'recommend' &&
+				<View style={styles.bookInfoWrap}>
+					<Text style={styles.bookTitleRecommend}>{this.props.title}</Text>
+					<Text style={styles.bookAuthorRecommend}>{this.props.teacherName}</Text>
+					<Text>하트 핀</Text>
+				</View>
+				}
+			</View>
+			<View style={[ { backgroundColor: this.props.bannerColor }, styles.itemContent ]}>
+				{this.props.type === 'best' &&
+				<Text style={styles.bookMemoBest} numberOfLines={3} ellipsizeMode={'tail'}>{this.props.memo}</Text>
+				}
+				{this.props.type === 'recommend' &&
+				<Text style={styles.bookMemoRecommend} numberOfLines={3} ellipsizeMode={'tail'}>{this.props.memo}</Text>
+				}
+			</View>
 			<View style={styles.bookThumbnail}>
-				<Image source={Dummy} style={styles.bookThumbnailSize}/>
+				<Image source={{ uri: this.props.bookThumbnail }} style={styles.bookThumbnailSize}/>
 			</View>
 		</View>
 	}
