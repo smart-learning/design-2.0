dd12371f814c8590c5032b553f1f304a39ee9259
nanuRuya <uru@grotesq.com>
2018-07-21 18:05:25 +0900

    회원가입 랜딩페이지 스크린 분리

---
+++ src/scripts/pages/auth/LandingScreen.js
@@ -0,0 +1,37 @@
+import React from "react";
+import {createStackNavigator} from "react-navigation";
+import {
+	NAV_OPTS_STACK_WITH_SEARCH,
+	NAV_OPTS_MAIN,
+	NAV_OPTS_DRAWER,
+	NAV_OPTS_COMMON,
+	NAV_OPTS_STACK
+} from "../../commons/nav";
+import SignUpLandingPage from "./SignUpLandingPage";
+import Policy from "./PolicyPage";
+import Privacy from "./PrivacyPage";
+
+
+const LandingScreen = createStackNavigator({
+
+		LandingPage: {
+			screen: SignUpLandingPage,
+		},
+
+		PolicyPage: {
+			screen: Policy,
+			navigationOptions: NAV_OPTS_STACK,
+		},
+
+		PrivacyPage: {
+			screen: Privacy,
+			navigationOptions: NAV_OPTS_STACK,
+		}
+	},
+
+	{ navigationOptions: NAV_OPTS_COMMON }
+);
+
+LandingScreen.navigationOptions = NAV_OPTS_DRAWER;
+
+export default LandingScreen;
