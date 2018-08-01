6d27918487044960cd1872838008160d741352fb
TaeHyun <taehyun.kim@influential.co.kr>
2018-08-01 11:33:17 +0900

    First iOS Commit  - CocoaPods 적용.  - 앱아이콘 추가.  - AppDelegate 수정.  - GoogleService 설정파일 추가.  - info.plist 수정.  - tvOS, Test프로젝트 등 당장 불필요한 default프로젝트 제거.  - RN과 통신할 native player 클래스 추가.  - CocoaPods을 init할 Makefile파일 추가.

--- ios/WelaaaV2/AppDelegate.m
+++ ios/WelaaaV2/AppDelegate.m
@@ -10,26 +10,102 @@
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
 
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+#import <KakaoOpenSDK/KakaoOpenSDK.h>
+
+#import <Fabric/Fabric.h>
+#import <Crashlytics/Crashlytics.h>
+
+@import Firebase;
+
+@interface AppDelegate ()
+
+@end
+
 @implementation AppDelegate
 
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+- (BOOL)          application : (UIApplication *) application
+didFinishLaunchingWithOptions : (NSDictionary *) launchOptions
 {
-  NSURL *jsCodeLocation;
+    NSURL *jsCodeLocation;
+  
+    jsCodeLocation = [ [RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot : @"index"
+                                                                     fallbackResource : nil     ];
+  
+    RCTRootView *rootView = [ [RCTRootView alloc] initWithBundleURL : jsCodeLocation
+                                                         moduleName : @"WelaaaV2"
+                                                  initialProperties : nil
+                                                      launchOptions : launchOptions ];
+  
+    rootView.backgroundColor = [ [UIColor alloc] initWithRed : 1.0f
+                                                       green : 1.0f
+                                                        blue : 1.0f
+                                                       alpha : 1   ];
+  
+    self.window = [ [UIWindow alloc] initWithFrame : [UIScreen mainScreen].bounds ];
+    UIViewController *rootViewController = [UIViewController new];
+    rootViewController.view = rootView;
+    self.window.rootViewController = rootViewController;
+    [self.window makeKeyAndVisible];
+  
+    [ [FBSDKApplicationDelegate sharedInstance] application : application
+                              didFinishLaunchingWithOptions : launchOptions ];
+  
+    [ Fabric with : @[[Crashlytics class]] ];
+    
+    // Use Firebase library to configure APIs
+    [FIRApp configure];
+  
+    return YES;
+}
 
-  jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
+- (BOOL) application : (UIApplication *) application
+             openURL : (NSURL *) url
+             options : (NSDictionary<UIApplicationOpenURLOptionsKey, id> *) options
+{
+    BOOL handled = [ [FBSDKApplicationDelegate sharedInstance] application : application
+                                                                   openURL : url
+                                                         sourceApplication : options[UIApplicationOpenURLOptionsSourceApplicationKey]
+                                                                annotation : options[UIApplicationOpenURLOptionsAnnotationKey]      ];
+  
+    if ( [KOSession isKakaoAccountLoginCallback : url] )
+     {
+     return [KOSession handleOpenURL : url];
+     }
+  
+    // Add any custom logic here.
+    return handled;
+}
 
-  RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
-                                                      moduleName:@"WelaaaV2"
-                                               initialProperties:nil
-                                                   launchOptions:launchOptions];
-  rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
+// 홈버튼 두번 눌렀을때, 인앱구매할때, 화면 쓸어내릴때, 화면 쓸어올릴때 등등.
+- (void) applicationWillResignActive : (UIApplication *) application
+{
+    UIView *colorView = [ [UIView alloc] initWithFrame : [self.window frame] ];
+    colorView.tag = 9999;
+    CGFloat hue = ( arc4random() % 256 / 256.0 );  //  0.0 to 1.0
+    CGFloat saturation = ( arc4random() % 128 / 256.0 ) + 0.5;  //  0.5 to 1.0, away from white
+    CGFloat brightness = ( arc4random() % 128 / 256.0 ) + 0.5;  //  0.5 to 1.0, away from black
+    UIColor *color = [UIColor colorWithHue : hue
+                                saturation : saturation
+                                brightness : brightness
+                                     alpha : 1        ];
+    colorView.backgroundColor = color;
+    [self.window addSubview : colorView];
+    [self.window bringSubviewToFront : colorView];
+}
 
-  self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
-  UIViewController *rootViewController = [UIViewController new];
-  rootViewController.view = rootView;
-  self.window.rootViewController = rootViewController;
-  [self.window makeKeyAndVisible];
-  return YES;
+//
+// 앱 활성화 로깅
+//
+- (void) applicationDidBecomeActive : (UIApplication *) application
+{
+    // Remove Multitask App Switcher Image.
+    UIView *colorView = [self.window viewWithTag : 9999];
+    [colorView removeFromSuperview];
+  
+    [FBSDKAppEvents activateApp];
+  
+    [KOSession handleDidBecomeActive];
 }
 
 @end
