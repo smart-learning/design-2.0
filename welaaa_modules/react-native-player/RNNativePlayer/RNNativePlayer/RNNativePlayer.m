ed9ac158fd657bcc18af20837d143d90c22e088c
KyeJune Hong <hohoyaing@gmail.com>
2018-08-13 23:24:32 +0900

    ignore풀에 ios 파일 일부제외

---
+++ welaaa_modules/react-native-player/RNNativePlayer/RNNativePlayer/RNNativePlayer.m
@@ -0,0 +1,13 @@
+//
+//  RNNativePlayer.m
+//  RNNativePlayer
+//
+//  Created by KyeJune Hong on 13/08/2018.
+//  Copyright © 2018 KyeJune Hong. All rights reserved.
+//
+
+#import "RNNativePlayer.h"
+
+@implementation RNNativePlayer
+
+@end
