905a006b0736e143f34329a3f0a607841987b7e4
KyeJune Hong <hohoyaing@gmail.com>
2018-08-14 11:41:34 +0900

    공유 기능 추가

--- src/scripts/commons/device.js
+++ src/scripts/commons/device.js
@@ -1,28 +1,54 @@
 import React from 'react';
-import {NativeModules, I18n, Platform} from 'react-native';
+import {NativeModules, Share, Platform} from 'react-native';
 import RNLocalizable from 'react-native-localizable';
 
 
 export default {
 
-	getLocale(){
+	getLocale() {
 
 		let locale = 'en';
 
-		if ( Platform.OS === 'android') {
+		if (Platform.OS === 'android') {
 			locale = NativeModules.I18nManager.localeIdentifier;
 		} else {
 			locale = NativeModules.SettingsManager.settings.AppleLocale.replace(/_/, '-');
 		}
 
-		return locale.substr( 0, 2 );
+		return locale.substr(0, 2);
 
 	},
 
-	getPlatformValue( name ){
-		let value = RNLocalizable[name]||null;
+	getPlatformValue(name) {
+		let value = RNLocalizable[name] || null;
 
 		return value;
+	},
+
+	share(title, message, url) {
+		Share.share(
+			{
+				...Platform.select({
+					ios: {
+						message: message,
+						url: url,
+					},
+
+					android: {
+						message: message,
+					}
+				}),
+
+				title: title,
+			},
+			{
+				...Platform.select({
+					ios: {},
+					android: {
+						dialogTitle: message + ' ' + url
+					}
+				})
+			})
 	}
 
 }
