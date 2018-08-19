b29ab322ee004e2a2c6d31841246baa62f8053af
nanuRuya <uru@grotesq.com>
2018-08-19 21:14:57 +0900

    윌라소개&이용가이드: 뷰, 목록 레이아웃 적용

--- src/scripts/pages/my/GuideViewPage.js
+++ src/scripts/pages/my/GuideViewPage.js
@@ -5,20 +5,36 @@ import Store from '../../../scripts/commons/store';
 import {SafeAreaView} from "react-navigation";
 
 const styles = StyleSheet.create({
-	//
+	title: {
+		backgroundColor: '#F6F6F6',
+		padding: 15,
+	},
+	titleText: {
+		fontSize: 15,
+		color: '#4a4a4a',
+	},
+	content: {
+		padding: 15,
+	},
+	contentText: {
+		fontSize: 14,
+		color: '#4a4a4a',
+	},
 });
 
 export default class GuideViewPage extends React.Component {
 
 	render() {
-		return <View style={CommonStyles.container}>
+		return <View style={[ CommonStyles.container, { backgroundColor: '#ffffff' } ]}>
 			<SafeAreaView style={{flex: 1, width: '100%'}}>
 				<ScrollView style={{flex: 1}}>
-					<Text>GuideViewPage 서브페이지</Text>
-					<Button
-						onPress={() => this.props.navigation.navigate('MyInfoHome')}
-						title="뒤로"
-					/>
+					{/*<Text>{this.props.navigation.state.params.id}</Text>*/}
+					<View style={styles.title}>
+						<Text style={styles.titleText}>title</Text>
+					</View>
+					<View style={styles.content}>
+						<Text style={styles.contentText}>content</Text>
+					</View>
 				</ScrollView>
 			</SafeAreaView>
 		</View>
