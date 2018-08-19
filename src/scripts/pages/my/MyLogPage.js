365886146b82bd1c8ba0c71dedc951b11e8ca3e2
nanuRuya <uru@grotesq.com>
2018-08-19 14:42:06 +0900

    활동로그보기 레이아웃 적용

--- src/scripts/pages/my/MyLogPage.js
+++ src/scripts/pages/my/MyLogPage.js
@@ -54,6 +54,53 @@ const styles = StyleSheet.create({
 	},
 	tabContentContainer: {
 		paddingTop: 40,
+	},
+	dayLabel: {
+		justifyContent: 'center',
+		alignItems: 'center',
+		marginTop: 15,
+		marginBottom: 20,
+		height: 24,
+		paddingLeft: 20,
+		paddingRight: 20,
+		borderWidth: 1,
+		borderColor: '#dbdbdb',
+	},
+	dayLabelText: {
+		fontSize: 13,
+		color: '#4a4a4a'
+	},
+	logItem: {
+		flexDirection: 'row',
+		marginBottom: 10,
+	},
+	logBullet: {
+		position: 'relative',
+		top: 7,
+		width: 3,
+		height: 3,
+		marginRight: 7,
+		backgroundColor: '#4a4a4a',
+	},
+	logTextContainer: {
+		flexDirection: 'row',
+	},
+	logTextPoint: {
+		position: 'relative',
+		top: 1,
+		marginRight: 5,
+		fontSize: 15,
+		color: '#26c281',
+	},
+	logText: {
+		marginRight: 5,
+		fontSize: 15,
+		color: '#4a4a4a',
+	},
+	logHr: {
+		width: '100%',
+		height: 1,
+		backgroundColor: '#dbdbdb',
 	}
 });
 
@@ -63,19 +110,275 @@ export default class MyLogPage extends React.Component {
 	@observable tabStatus = 'me';
 
 	render() {
-		return <View style={CommonStyles.container}>
+		return <View style={[ CommonStyles.container, { backgroundColor: '#ffffff' } ]}>
 			<SafeAreaView style={{flex: 1, width: '100%'}}>
 				<ScrollView style={{flex: 1}}>
 					<View style={styles.tabContentContainer}>
 						{this.tabStatus === 'me' &&
-						<View style={styles.tabContent}>
-							<Text>나의 활동 컨텐츠</Text>
+						<View style={[CommonStyles.contentContainer, styles.tabContent]}>
+							<View style={styles.logSection}>
+								<View>
+									<View style={styles.dayLabel} borderRadius={12}>
+										<Text style={styles.dayLabelText}>날짜라벨</Text>
+									</View>
+								</View>
+								<View style={styles.logItem}>
+									<View style={styles.logBullet}/>
+									<View>
+										<View style={styles.logTextContainer}>
+											<TouchableOpacity activeOpacity={0.9}>
+												<Text style={styles.logTextPoint}>김딸기</Text>
+											</TouchableOpacity>
+											<Text style={styles.logText}>님이</Text>
+											<TouchableOpacity activeOpacity={0.9}>
+												<Text style={styles.logTextPoint}>타이틀</Text>
+											</TouchableOpacity>
+											<Text style={styles.logText}>강좌를 스크랩 했습니다.</Text>
+										</View>
+									</View>
+								</View>
+
+								<View style={styles.logItem}>
+									<View style={styles.logBullet}/>
+									<View>
+										<View style={styles.logTextContainer}>
+											<TouchableOpacity activeOpacity={0.9}>
+												<Text style={styles.logTextPoint}>김딸기</Text>
+											</TouchableOpacity>
+											<Text style={styles.logText}>님이</Text>
+											<TouchableOpacity activeOpacity={0.9}>
+												<Text style={styles.logTextPoint}>타이틀</Text>
+											</TouchableOpacity>
+											<Text style={styles.logText}>강의를 스크랩 했습니다.</Text>
+										</View>
+									</View>
+								</View>
+
+								<View style={styles.logItem}>
+									<View style={styles.logBullet}/>
+									<View>
+										<View style={styles.logTextContainer}>
+											<TouchableOpacity activeOpacity={0.9}>
+												<Text style={styles.logTextPoint}>김딸기</Text>
+											</TouchableOpacity>
+											<Text style={styles.logText}>님이</Text>
+											<TouchableOpacity activeOpacity={0.9}>
+												<Text style={styles.logTextPoint}>타이틀</Text>
+											</TouchableOpacity>
+											<Text style={styles.logText}>오디오북을 스크랩 했습니다.</Text>
+										</View>
+									</View>
+								</View>
+
+								<View style={styles.logItem}>
+									<View style={styles.logBullet}/>
+									<View>
+										<View style={styles.logTextContainer}>
+											<TouchableOpacity activeOpacity={0.9}>
+												<Text style={styles.logTextPoint}>김딸기</Text>
+											</TouchableOpacity>
+											<Text style={styles.logText}>님이</Text>
+											<TouchableOpacity activeOpacity={0.9}>
+												<Text style={styles.logTextPoint}>타이틀</Text>
+											</TouchableOpacity>
+											<Text style={styles.logText}>오디오북을 구매하셨습니다.</Text>
+										</View>
+									</View>
+								</View>
+
+								<View style={styles.logItem}>
+									<View style={styles.logBullet}/>
+									<View>
+										<View style={styles.logTextContainer}>
+											<TouchableOpacity activeOpacity={0.9}>
+												<Text style={styles.logTextPoint}>김딸기</Text>
+											</TouchableOpacity>
+											<Text style={styles.logText}>님이</Text>
+											<TouchableOpacity activeOpacity={0.9}>
+												<Text style={styles.logTextPoint}>타이틀</Text>
+											</TouchableOpacity>
+											<Text style={styles.logText}>오디오북을 좋아합니다.</Text>
+										</View>
+									</View>
+								</View>
+
+								<View style={styles.logItem}>
+									<View style={styles.logBullet}/>
+									<View>
+										<View style={styles.logTextContainer}>
+											<TouchableOpacity activeOpacity={0.9}>
+												<Text style={styles.logTextPoint}>김딸기</Text>
+											</TouchableOpacity>
+											<Text style={styles.logText}>님이 태그이름(을)를</Text>
+											<TouchableOpacity activeOpacity={0.9}>
+												<Text style={styles.logTextPoint}>관심태그</Text>
+											</TouchableOpacity>
+											<Text style={styles.logText}>에 추가했습니다.</Text>
+										</View>
+									</View>
+								</View>
+
+								<View style={styles.logItem}>
+									<View style={styles.logBullet}/>
+									<View>
+										<View style={styles.logTextContainer}>
+											<TouchableOpacity activeOpacity={0.9}>
+												<Text style={styles.logTextPoint}>김딸기</Text>
+											</TouchableOpacity>
+											<Text style={styles.logText}>님이 멤버십 회원이 되셨습니다!</Text>
+										</View>
+									</View>
+								</View>
+
+								<View style={styles.logItem}>
+									<View style={styles.logBullet}/>
+									<View>
+										<View style={styles.logTextContainer}>
+											<TouchableOpacity activeOpacity={0.9}>
+												<Text style={styles.logTextPoint}>김딸기</Text>
+											</TouchableOpacity>
+											<Text style={styles.logText}>님이 윌라의 회원이 되셨습니다!</Text>
+										</View>
+									</View>
+								</View>
+
+								<View style={styles.logHr}/>
+
+							</View>
 						</View>
 						}
 
 						{this.tabStatus === 'friend' &&
 						<View style={styles.tabContent}>
-							<Text>친구의 활동 컨텐츠</Text>
+							<View style={styles.logSection}>
+								<View>
+									<View style={styles.dayLabel} borderRadius={12}>
+										<Text style={styles.dayLabelText}>날짜라벨</Text>
+									</View>
+								</View>
+								<View style={styles.logItem}>
+									<View style={styles.logBullet}/>
+									<View>
+										<View style={styles.logTextContainer}>
+											<TouchableOpacity activeOpacity={0.9}>
+												<Text style={styles.logTextPoint}>김딸기</Text>
+											</TouchableOpacity>
+											<Text style={styles.logText}>님이</Text>
+											<TouchableOpacity activeOpacity={0.9}>
+												<Text style={styles.logTextPoint}>타이틀</Text>
+											</TouchableOpacity>
+											<Text style={styles.logText}>강좌를 스크랩 했습니다.</Text>
+										</View>
+									</View>
+								</View>
+
+								<View style={styles.logItem}>
+									<View style={styles.logBullet}/>
+									<View>
+										<View style={styles.logTextContainer}>
+											<TouchableOpacity activeOpacity={0.9}>
+												<Text style={styles.logTextPoint}>김딸기</Text>
+											</TouchableOpacity>
+											<Text style={styles.logText}>님이</Text>
+											<TouchableOpacity activeOpacity={0.9}>
+												<Text style={styles.logTextPoint}>타이틀</Text>
+											</TouchableOpacity>
+											<Text style={styles.logText}>강의를 스크랩 했습니다.</Text>
+										</View>
+									</View>
+								</View>
+
+								<View style={styles.logItem}>
+									<View style={styles.logBullet}/>
+									<View>
+										<View style={styles.logTextContainer}>
+											<TouchableOpacity activeOpacity={0.9}>
+												<Text style={styles.logTextPoint}>김딸기</Text>
+											</TouchableOpacity>
+											<Text style={styles.logText}>님이</Text>
+											<TouchableOpacity activeOpacity={0.9}>
+												<Text style={styles.logTextPoint}>타이틀</Text>
+											</TouchableOpacity>
+											<Text style={styles.logText}>오디오북을 스크랩 했습니다.</Text>
+										</View>
+									</View>
+								</View>
+
+								<View style={styles.logItem}>
+									<View style={styles.logBullet}/>
+									<View>
+										<View style={styles.logTextContainer}>
+											<TouchableOpacity activeOpacity={0.9}>
+												<Text style={styles.logTextPoint}>김딸기</Text>
+											</TouchableOpacity>
+											<Text style={styles.logText}>님이</Text>
+											<TouchableOpacity activeOpacity={0.9}>
+												<Text style={styles.logTextPoint}>타이틀</Text>
+											</TouchableOpacity>
+											<Text style={styles.logText}>오디오북을 구매하셨습니다.</Text>
+										</View>
+									</View>
+								</View>
+
+								<View style={styles.logItem}>
+									<View style={styles.logBullet}/>
+									<View>
+										<View style={styles.logTextContainer}>
+											<TouchableOpacity activeOpacity={0.9}>
+												<Text style={styles.logTextPoint}>김딸기</Text>
+											</TouchableOpacity>
+											<Text style={styles.logText}>님이</Text>
+											<TouchableOpacity activeOpacity={0.9}>
+												<Text style={styles.logTextPoint}>타이틀</Text>
+											</TouchableOpacity>
+											<Text style={styles.logText}>오디오북을 좋아합니다.</Text>
+										</View>
+									</View>
+								</View>
+
+								<View style={styles.logItem}>
+									<View style={styles.logBullet}/>
+									<View>
+										<View style={styles.logTextContainer}>
+											<TouchableOpacity activeOpacity={0.9}>
+												<Text style={styles.logTextPoint}>김딸기</Text>
+											</TouchableOpacity>
+											<Text style={styles.logText}>님이 태그이름(을)를</Text>
+											<TouchableOpacity activeOpacity={0.9}>
+												<Text style={styles.logTextPoint}>관심태그</Text>
+											</TouchableOpacity>
+											<Text style={styles.logText}>에 추가했습니다.</Text>
+										</View>
+									</View>
+								</View>
+
+								<View style={styles.logItem}>
+									<View style={styles.logBullet}/>
+									<View>
+										<View style={styles.logTextContainer}>
+											<TouchableOpacity activeOpacity={0.9}>
+												<Text style={styles.logTextPoint}>김딸기</Text>
+											</TouchableOpacity>
+											<Text style={styles.logText}>님이 멤버십 회원이 되셨습니다!</Text>
+										</View>
+									</View>
+								</View>
+
+								<View style={styles.logItem}>
+									<View style={styles.logBullet}/>
+									<View>
+										<View style={styles.logTextContainer}>
+											<TouchableOpacity activeOpacity={0.9}>
+												<Text style={styles.logTextPoint}>김딸기</Text>
+											</TouchableOpacity>
+											<Text style={styles.logText}>님이 윌라의 회원이 되셨습니다!</Text>
+										</View>
+									</View>
+								</View>
+
+								<View style={styles.logHr}/>
+
+							</View>
 						</View>
 						}
 					</View>
