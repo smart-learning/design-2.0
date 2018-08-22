e593f70c02940ebbaba499273a70f825c279849e
TaeHyun <taehyun.kim@influential.co.kr>
2018-08-22 15:10:16 +0900

    Contents List Pop-up View 소스 일괄 추가.  - 1.0 소스를 그대로 add하였고 리스트 버튼 이벤트에 대한 구현을 시작할 예정임.

---
+++ ios/WelaaaV2/Test/ContentsListView/ContentsListPopupView.h
@@ -0,0 +1,31 @@
+
+#import <UIKit/UIKit.h>
+
+#import "UIImage+TintColor.h"
+#import "ContentsListPopupViewCell.h"
+#import "ApiManager.h"
+
+@protocol ContentsListPopupViewDelegate;
+
+@interface ContentsListPopupView : UIView
+
+@property (nonatomic, weak) id <ContentsListPopupViewDelegate> delegate;
+@property (nonatomic, strong) NSArray *playList;
+@property (nonatomic, assign) BOOL isAudioContentType;
+@property (nonatomic, assign) NSInteger currentPlayIndex;
+@property (nonatomic, assign) BOOL isAuthor;
+
+- (void) start;
+- (void) setTitle : (NSString *) title;
+- (void) reOrderSubviews;
+@end
+
+@protocol ContentsListPopupViewDelegate <NSObject>
+@optional
+
+- (void) playListPopupView : (ContentsListPopupView *) view
+                 closeView : (id) sender;
+- (void) playListPopupView : (ContentsListPopupView *) view
+        selectedOtherIndex : (NSInteger) index;
+
+@end
