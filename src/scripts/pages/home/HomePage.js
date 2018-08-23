d78a5d46e7f98cf87c0206361f5e34f52e1b32cb
nanuRuya <uru@grotesq.com>
2018-08-23 21:58:00 +0900

    메인 탭 레이아웃 추가.

--- src/scripts/pages/home/HomePage.js
+++ src/scripts/pages/home/HomePage.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { observable } from 'mobx';
-import { observer } from 'mobx-react';
+import {observable} from 'mobx';
+import {observer} from 'mobx-react';
 import CommonStyles from "../../../styles/common";
 import Swiper from 'react-native-swiper';
 import {
@@ -10,7 +10,7 @@ import {
 	ImageBackground,
 	ScrollView,
 	StyleSheet,
-	Text,
+	Text, TextInput,
 	TouchableOpacity,
 	View
 } from "react-native";
@@ -23,254 +23,143 @@ import PageCategory from "../../components/PageCategory";
 import ClassList from "../../components/home/ClassList";
 import ClipRank from "../../components/home/ClipRank";
 import ClassContinueList from "../../components/home/ClassContinueList";
+import IcSearch from "../../../images/ic-my-friend-search.png";
+import IcFb from "../../../images/ic-my-friend-fb.png";
+import IcBook from "../../../images/ic-my-friend-book.png";
+import DummyProfile from "../../../images/dummy-my-profile.png";
+import {SafeAreaView} from "react-navigation";
+import createStore from "../../commons/createStore";
+import HomeVideoPage from "./HomeVideoPage";
 
-const styles = StyleSheet.create( {
-	wrapper: {},
-	slide: {
-		flex: 1,
+const styles = StyleSheet.create({
+	tabContainer: {
+		position: 'absolute',
+		alignSelf: 'flex-start',
+		top: 0,
+		left: 0,
+		width: '100%',
+		height: 40,
+		backgroundColor: '#ffffff'
+	},
+	tabFlex: {
+		flexDirection: 'row',
+	},
+	tabItemContainer: {
+		width: '50%',
+	},
+	tabItem: {
 		justifyContent: 'center',
 		alignItems: 'center',
+		position: 'relative',
+		height: 40,
 	},
-	thumbnail: {
-		width: '100%',
-		paddingTop: '21%',
-		paddingBottom: '21%',
-	},
-	mainTitleCenter: {
-		textAlign: 'center',
-	},
-	titleH2: {
-		fontSize: 26,
-		fontWeight: 'bold',
-		color: '#333333',
-	},
-	titleH4: {
-		paddingTop: 10,
-		fontSize: 13,
-		color: '#888888',
-	},
-	titleH3: {
-		fontSize: 13,
-		fontWeight: 'bold',
-		color: '#333333',
-	},
-	titleParagraph: {
-		paddingLeft: 15,
-		fontSize: 11,
-		color: '#b7b7b7',
-	},
-	titleLink: {
-		fontSize: 13,
-		color: CommonStyles.COLOR_PRIMARY,
-	},
-	titleHr: {
-		height: 1,
-		marginTop: 7,
-		backgroundColor: '#cecece',
-	},
-	linkViewAll: {
-		alignItems: 'center',
-		justifyContent: 'center',
-		width: '100%',
-		height: 36,
-		marginLeft: 'auto',
-		marginRight: 'auto',
-		backgroundColor: '#dddddd',
-	},
-	classLinkViewAll: {
-		marginTop: 15,
-		marginBottom: 30,
-	},
-	linkViewAllText: {
+	tabText: {
 		fontSize: 14,
-		color: '#888888',
+		color: '#a4a4a4',
 	},
-	linkViewAllIcon: {
-		paddingLeft: 7,
-		height: 13,
+	tabTextActive: {
+		fontSize: 14,
+		color: '#000000',
 	},
-	seriesContainer: {
-		paddingTop: 50,
-		paddingBottom: 50,
+	tabHr: {
+		position: 'absolute',
+		left: 0,
+		bottom: 0,
+		width: '100%',
+		height: 3,
+		backgroundColor: '#ffffff',
 	},
-	classContainer: {
-		paddingTop: 50,
-		paddingBottom: 50,
+	tabHrActive: {
+		position: 'absolute',
+		left: 0,
+		bottom: 0,
+		width: '100%',
+		height: 3,
+		backgroundColor: '#26c281',
 	},
-	classCategory: {
-		marginTop: 20,
-		marginBottom: 20,
+	tabContentContainer: {
+		paddingTop: 40,
 	},
-	classCategoryHr: {
-		height: 1,
-		backgroundColor: '#cecece',
-	},
-	seriesComponent: {
-		paddingTop: 30,
-	}
-} );
+});
 
 @observer
 class HomePage extends React.Component {
 
-	@observable slideHeight = null;
-	@observable clipRankContentSize = null;
-	@observable homeSeriesData = {};
-	@observable videoCategoryData = {};
-	@observable classHotData = {};
-	@observable classNewData = {};
-	@observable classRecommendData = {};
-	@observable clipRankData = {};
+	@observable tabStatus = 'video';
+
+	store = createStore({
+		slideHeight: null,
+		windowWidth: null,
+		clipRankContentSize: null,
+		homeSeriesData: {},
+		videoCategoryData: {},
+		classHotData: {},
+		classNewData: {},
+		classRecommendData: {},
+		clipRankData: {},
+	});
 
 	getData = async () => {
-		this.homeSeriesData = await net.getHomeSeries();
-		this.videoCategoryData = await net.getLectureCategory();
-		this.classHotData = await net.getHomeClassHot();
-		this.classNewData = await net.getHomeClassNew();
-		this.classRecommendData = await net.getHomeClassRecommend();
-		this.clipRankData = await net.getHomeClipRank();
-
-		console.log( 'get series', this.homeSeriesData );
+		this.store.homeSeriesData = await net.getHomeSeries();
+		this.store.videoCategoryData = await net.getLectureCategory();
+		this.store.classHotData = await net.getHomeClassHot();
+		this.store.classNewData = await net.getHomeClassNew();
+		this.store.classRecommendData = await net.getHomeClassRecommend();
+		this.store.clipRankData = await net.getHomeClipRank();
 	};
 
 	componentDidMount() {
-		let windowWidth = Dimensions.get( 'window' ).width;
+		let windowWidth = Dimensions.get('window').width;
 
-		this.slideHeight = windowWidth * 0.42;
-		this.clipRankContentSize = windowWidth - 85;
+		this.store.windowWidth = windowWidth;
+		this.store.slideHeight = windowWidth * 0.42;
+		this.store.clipRankContentSize = windowWidth - 85;
 
 		this.getData();
 	}
 
 	render() {
-		return <View style={[ CommonStyles.container, { backgroundColor: '#ffffff' } ]}>
-			<ScrollView style={{ width: '100%' }}>
-				{/* 이미지 스와이퍼 */}
-				<View style={{ height: this.slideHeight }}>
-					<Swiper style={styles.wrapper}
-							showsButtons={false}
-							height={window.width}
-							paginationStyle={{ bottom: 10 }}>
-						<View style={styles.slide}>
-							<ImageBackground source={Dummy1} resizeMode="cover" style={styles.thumbnail}/>
+		return <View style={[CommonStyles.container, {backgroundColor: '#ffffff'}]}>
+			<SafeAreaView style={{flex: 1, width: '100%'}}>
+				<ScrollView style={{flex: 1}}>
+					{this.tabStatus === 'video' &&
+					<View style={styles.tabContentContainer}>
+						<HomeVideoPage store={this.store}/>
+					</View>
+					}
+
+					{this.tabStatus === 'audioBook' &&
+					<View style={styles.tabContentContainer}>
+						<Text>audio</Text>
+					</View>
+					}
+				</ScrollView>
+				<View style={styles.tabContainer}>
+					<View style={styles.tabFlex}>
+						<View style={styles.tabItemContainer}>
+							<TouchableOpacity activeOpacity={0.9} onPress={() => this.tabStatus = 'video'}>
+								<View style={styles.tabItem}>
+									<Text style={this.tabStatus === 'video' ? styles.tabTextActive : styles.tabText}>
+										비디오
+									</Text>
+									<View style={this.tabStatus === 'video' ? styles.tabHrActive : styles.tabHr}/>
+								</View>
+							</TouchableOpacity>
 						</View>
-						<View style={styles.slide}>
-							<ImageBackground source={Dummy2} resizeMode="cover" style={styles.thumbnail}/>
+						<View style={styles.tabItemContainer}>
+							<TouchableOpacity activeOpacity={0.9} onPress={() => this.tabStatus = 'audioBook'}>
+								<View style={styles.tabItem}>
+									<Text
+										style={this.tabStatus === 'audioBook' ? styles.tabTextActive : styles.tabText}>
+										오디오북
+									</Text>
+									<View style={this.tabStatus === 'audioBook' ? styles.tabHrActive : styles.tabHr}/>
+								</View>
+							</TouchableOpacity>
 						</View>
-					</Swiper>
+					</View>
 				</View>
-				{/* /이미지 스와이퍼 */}
-
-				{this.homeSeriesData.length > 6 &&
-				<View style={[ CommonStyles.contentContainer, styles.seriesContainer ]}>
-					<View>
-						<Text style={[ styles.mainTitleCenter, styles.titleH2 ]}>
-							윌라 추천 시리즈
-						</Text>
-						<Text style={[ styles.mainTitleCenter, styles.titleH4 ]}>
-							당신이 배우고 싶은 모든 것
-						</Text>
-					</View>
-
-					<View style={styles.seriesComponent}>
-						<Series itemData={this.homeSeriesData}/>
-					</View>
-
-					<TouchableOpacity activeOpacity={0.9}>
-						<View style={styles.linkViewAll} borderRadius={5}>
-							<Text style={styles.linkViewAllText}>
-								추천 시리즈 전체 보기 <Image source={IcAngleRightGrey} style={styles.linkViewAllIcon}/>
-							</Text>
-						</View>
-					</TouchableOpacity>
-				</View>
-				}
-
-				<View style={[ CommonStyles.contentContainer, styles.classContainer ]}>
-					<View>
-						<Text style={[ styles.mainTitleCenter, styles.titleH2 ]}>
-							윌라 프리미엄 클래스
-						</Text>
-						<Text style={[ styles.mainTitleCenter, styles.titleH4 ]}>
-							당신의 커리어 성공과 행복한 일상을 위한 교육
-						</Text>
-					</View>
-
-					<View style={styles.classCategory}>
-						<View style={styles.classCategoryHr}/>
-						<PageCategory data={this.videoCategoryData.items}/>
-						<View style={styles.classCategoryHr}/>
-					</View>
-
-					<View style={CommonStyles.alignJustifyContentBetween}>
-						<Text style={styles.titleH3}>
-							회원들이 듣고 있는 클래스
-						</Text>
-						<Text style={[ styles.titleParagraph, { marginLeft: 0 } ]}>
-							2018. 07.09 업데이트
-						</Text>
-					</View>
-
-					<ClassList classType="hot" itemData={this.classHotData}/>
-
-					<View style={CommonStyles.alignJustifyContentBetween}>
-						<Text style={styles.titleH3}>
-							새로 나온 클래스
-						</Text>
-						<Text style={[ styles.titleParagraph, { marginLeft: 0 } ]}>
-							2018. 07.09 업데이트
-						</Text>
-					</View>
-
-					<ClassList itemData={this.classNewData}/>
-
-					<View style={CommonStyles.alignJustifyContentBetween}>
-						<Text style={styles.titleH3}>
-							윌라 추천 클래스
-						</Text>
-						<Text style={[ styles.titleParagraph, { marginLeft: 0 } ]}>
-							2018. 07.09 업데이트
-						</Text>
-					</View>
-
-					<ClassList itemData={this.classRecommendData}/>
-
-					<TouchableOpacity activeOpacity={0.9}>
-						<View style={[ styles.linkViewAll, styles.classLinkViewAll ]} borderRadius={5}>
-							<Text style={styles.linkViewAllText}>
-								클래스 전체 보기 <Image source={IcAngleRightGrey} style={styles.linkViewAllIcon}/>
-							</Text>
-						</View>
-					</TouchableOpacity>
-
-					<View style={CommonStyles.alignJustifyContentBetween}>
-						<Text style={styles.titleH3}>
-							지금 많이 듣고 있는 강의클립
-						</Text>
-						<Text style={[ styles.titleParagraph, { marginLeft: 'auto' } ]}>
-							2018. 07.09 업데이트
-						</Text>
-					</View>
-					<View style={styles.titleHr}/>
-
-					<ClipRank itemData={this.clipRankData} clipRankContentSize={this.clipRankContentSize}/>
-
-					<View style={CommonStyles.alignJustifyItemCenter}>
-						<Text style={styles.titleH3}>
-							이어보기
-						</Text>
-						<Text style={styles.titleParagraph}>
-							2018. 07.09 업데이트
-						</Text>
-						<TouchableOpacity activeOpacity={0.9} style={{ marginLeft: 'auto' }}>
-							<Text style={styles.titleLink}>마이윌라</Text>
-						</TouchableOpacity>
-					</View>
-					<View style={styles.titleHr}/>
-
-					<ClassContinueList/>
-				</View>
-			</ScrollView>
+			</SafeAreaView>
 		</View>
 	}
 }
