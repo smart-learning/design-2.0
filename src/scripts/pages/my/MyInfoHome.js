301edc37acc5a20252108f6b56ad8bc5fa4d8313
nanuRuya <uru@grotesq.com>
2018-08-27 15:45:00 +0900

    마이윌라 항목 정리 #50

--- src/scripts/pages/my/MyInfoHome.js
+++ src/scripts/pages/my/MyInfoHome.js
@@ -167,21 +167,18 @@ export default class MyInfoHome extends React.Component {
 
 		const {navigation} = this.props;
 
-		return <View style={[CommonStyles.container, {backgroundColor: '#F5F5F5', flex:1 }]}>
-			<SafeAreaView style={ { flex:1, width:'100%'} }>
-				<ScrollView style={ { flex:1 } }>
+		return <View style={[CommonStyles.container, {backgroundColor: '#F5F5F5', flex: 1}]}>
+			<SafeAreaView style={{flex: 1, width: '100%'}}>
+				<ScrollView style={{flex: 1}}>
 					<ImageBackground source={BgMy} style={styles.myInfo}>
 
 						<View style={[CommonStyles.alignJustifyContentBetween, styles.myHeader]}>
-							{/*<Button title="홈으로.."*/}
-									{/*onPress={() => navigation.navigate('HomeScreen')}*/}
-							{/*/>*/}
 							<HomeButton/>
 
 							<Text style={styles.myHeaderTitle}>마이윌라</Text>
 
 							<TouchableOpacity activeOpacity={0.9}
-											  onPress={() => navigation.navigate('SetAppPage', { title:'설정' } )}
+											  onPress={() => navigation.navigate('SetAppPage', {title: '설정'})}
 							>
 								<Image source={IcCog} style={styles.myHeaderIcon}/>
 							</TouchableOpacity>
@@ -197,12 +194,15 @@ export default class MyInfoHome extends React.Component {
 										<Text>package</Text>
 									</View>
 								</View>
+								{/*고객사 요청으로 화면에서 감춤 처리*/}
+								{1 === 2 &&
 								<View style={[styles.myInfoContentItem, styles.myInfoParagraphContainer]}>
 									<Text style={styles.myInfoParagraph}>한줄 메세지를 작성해주세요~</Text>
 									<TouchableOpacity activeOpacity={0.9}>
 										<Text style={styles.myInfoParagraphButton}>쓰기</Text>
 									</TouchableOpacity>
 								</View>
+								}
 								<Text style={styles.myInfoEmail}>mail@mail.com</Text>
 							</View>
 						</View>
@@ -212,7 +212,7 @@ export default class MyInfoHome extends React.Component {
 						<Text style={styles.sectionTitle}>콘텐츠 이용 현황</Text>
 
 						<TouchableOpacity activeOpacity={0.9}
-										  onPress={() => navigation.navigate('LectureUsePage', { title:'최근 재생중 강좌'} )}>
+										  onPress={() => navigation.navigate('LectureUsePage', {title: '최근 재생중 강좌'})}>
 							<View style={styles.listItem}>
 								<Image source={IcPlay} style={styles.listItemIcon}/>
 								<Text style={styles.listItemTitle}>최근 재생중 강좌</Text>
@@ -224,7 +224,7 @@ export default class MyInfoHome extends React.Component {
 							<View style={styles.listItemBarBullet}/>
 						</View>
 						<TouchableOpacity activeOpacity={0.9}
-										  onPress={() => navigation.navigate('LectureBuyPage', { title:'구매한 강좌'} )}>
+										  onPress={() => navigation.navigate('LectureBuyPage', {title: '구매한 강좌'})}>
 							<View style={styles.listItem}>
 								<Image source={IcPlay} style={styles.listItemIcon}/>
 								<Text style={styles.listItemTitle}>구매한 강좌</Text>
@@ -236,7 +236,7 @@ export default class MyInfoHome extends React.Component {
 							<View style={styles.listItemBarBullet}/>
 						</View>
 						<TouchableOpacity activeOpacity={0.9}
-										  onPress={() => navigation.navigate('AudioBookTicketPage', { title:'나의 오디오북 이용권'} )}>
+										  onPress={() => navigation.navigate('AudioBookTicketPage', {title: '나의 오디오북 이용권'})}>
 							<View style={styles.listItem}>
 								<Image source={IcMusic} style={styles.listItemIcon}/>
 								<Text style={styles.listItemTitle}>나의 오디오북 이용권</Text>
@@ -248,7 +248,7 @@ export default class MyInfoHome extends React.Component {
 							<View style={styles.listItemBarBullet}/>
 						</View>
 						<TouchableOpacity activeOpacity={0.9}
-										  onPress={() => navigation.navigate('AudioBookUsePage', { title:'이용중 오디오북'} )}>
+										  onPress={() => navigation.navigate('AudioBookUsePage', {title: '이용중 오디오북'})}>
 							<View style={styles.listItem}>
 								<Image source={IcMusic} style={styles.listItemIcon}/>
 								<Text style={styles.listItemTitle}>이용중 오디오북</Text>
@@ -260,7 +260,7 @@ export default class MyInfoHome extends React.Component {
 							<View style={styles.listItemBarBullet}/>
 						</View>
 						<TouchableOpacity activeOpacity={0.9}
-										  onPress={() => navigation.navigate('AudioBookBuyPage', { title:'구매한 오디오북'} )}>
+										  onPress={() => navigation.navigate('AudioBookBuyPage', {title: '구매한 오디오북'})}>
 							<View style={styles.listItem}>
 								<Image source={IcMusic} style={styles.listItemIcon}/>
 								<Text style={styles.listItemTitle}>구매한 오디오북</Text>
@@ -272,7 +272,7 @@ export default class MyInfoHome extends React.Component {
 							<View style={styles.listItemBarBullet}/>
 						</View>
 						<TouchableOpacity activeOpacity={0.9}
-										  onPress={() => navigation.navigate('DownloadContentPage', { title:'다운로드 컨텐츠'} )}>
+										  onPress={() => navigation.navigate('DownloadContentPage', {title: '다운로드 컨텐츠'})}>
 							<View style={styles.listItem}>
 								<Image source={IcDownload} style={styles.listItemIcon}/>
 								<Text style={styles.listItemTitle}>다운로드 컨텐츠</Text>
@@ -283,63 +283,75 @@ export default class MyInfoHome extends React.Component {
 							<View style={styles.listItemBar}/>
 							<View style={styles.listItemBarBullet}/>
 						</View>
-						<TouchableOpacity activeOpacity={0.9}
-										  onPress={() => navigation.navigate('SetTagPage', { title:'관심태그 설정하기'} )}>
-							<View style={styles.listItem}>
-								<Image source={IcTag} style={styles.listItemIcon}/>
-								<Text style={styles.listItemTitle}>관심태그 설정하기</Text>
-								<Image source={IcAngleRight} style={[styles.listItemBullet, {marginLeft: 'auto'}]}/>
+						{/*고객사 요청으로 화면에서 감춤 처리*/}
+						{1 === 2 && <View>
+							<TouchableOpacity activeOpacity={0.9}
+											  onPress={() => navigation.navigate('SetTagPage', {title: '관심태그 설정하기'})}>
+								<View style={styles.listItem}>
+									<Image source={IcTag} style={styles.listItemIcon}/>
+									<Text style={styles.listItemTitle}>관심태그 설정하기</Text>
+									<Image source={IcAngleRight} style={[styles.listItemBullet, {marginLeft: 'auto'}]}/>
+								</View>
+							</TouchableOpacity>
+							< View style={styles.listItemBarContainer}>
+								<View style={styles.listItemBar}/>
+								<View style={styles.listItemBarBullet}/>
 							</View>
-						</TouchableOpacity>
-						<View style={styles.listItemBarContainer}>
-							<View style={styles.listItemBar}/>
-							<View style={styles.listItemBarBullet}/>
 						</View>
+						}
 						<TouchableOpacity activeOpacity={0.9}
-										  onPress={() => navigation.navigate('ClipPlayListPage', { title:'최근 재생 강의클립 목록'} )}>
+										  onPress={() => navigation.navigate('ClipPlayListPage', {title: '최근 재생 강의클립 목록'})}>
 							<View style={styles.listItem}>
 								<Image source={IcPlay} style={styles.listItemIcon}/>
 								<Text style={styles.listItemTitle}>최근 재생 강의클립 목록</Text>
 								<Image source={IcAngleRight} style={[styles.listItemBullet, {marginLeft: 'auto'}]}/>
 							</View>
 						</TouchableOpacity>
-						<View style={styles.listItemBarContainer}>
-							<View style={styles.listItemBar}/>
-							<View style={styles.listItemBarBullet}/>
+						{/*고객사 요청으로 화면에서 감춤 처리*/}
+						{1 === 2 && <View>
+							<View style={styles.listItemBarContainer}>
+								<View style={styles.listItemBar}/>
+								<View style={styles.listItemBarBullet}/>
+							</View>
+							<TouchableOpacity activeOpacity={0.9}
+											  onPress={() => navigation.navigate('MyLogPage', {title: '활동로그 보기'})}>
+								<View style={styles.listItem}>
+									<Image source={IcProfile} style={styles.listItemIcon}/>
+									<Text style={styles.listItemTitle}>활동로그 보기</Text>
+									<Image source={IcAngleRight} style={[styles.listItemBullet, {marginLeft: 'auto'}]}/>
+								</View>
+							</TouchableOpacity>
 						</View>
-						<TouchableOpacity activeOpacity={0.9}
-										  onPress={() => navigation.navigate('MyLogPage', { title:'활동로그 보기'} )}>
-							<View style={styles.listItem}>
-								<Image source={IcProfile} style={styles.listItemIcon}/>
-								<Text style={styles.listItemTitle}>활동로그 보기</Text>
-								<Image source={IcAngleRight} style={[styles.listItemBullet, {marginLeft: 'auto'}]}/>
-							</View>
-						</TouchableOpacity>
+						}
 					</View>
-					<View style={{height: 8}}/>
-					<View style={styles.sectionLayout}>
-						<Text style={styles.sectionTitle}>윌라친구</Text>
-						<Text style={styles.friendParagraph}>친구와 함께하면 스마트러닝이 더 즐겁습니다!</Text>
-						<TouchableOpacity activeOpacity={0.9}
-										  onPress={() => navigation.navigate('FriendPage', { title:'친구보기'} )}>
-							<View style={styles.friendButton} borderRadius={5}>
-								<Text style={styles.friendButtonText}>친구보기</Text>
-							</View>
-						</TouchableOpacity>
+					{/*고객사 요청으로 화면에서 감춤 처리*/}
+					{1 === 2 && <View>
+						<View style={{height: 8}}/>
+						<View style={styles.sectionLayout}>
+							<Text style={styles.sectionTitle}>윌라친구</Text>
+							<Text style={styles.friendParagraph}>친구와 함께하면 스마트러닝이 더 즐겁습니다!</Text>
+							<TouchableOpacity activeOpacity={0.9}
+											  onPress={() => navigation.navigate('FriendPage', {title: '친구보기'})}>
+								<View style={styles.friendButton} borderRadius={5}>
+									<Text style={styles.friendButtonText}>친구보기</Text>
+								</View>
+							</TouchableOpacity>
+						</View>
 					</View>
+					}
 					<View style={{height: 8}}/>
 					<View style={styles.sectionLayout}>
 						<Text style={styles.sectionTitle}>알림 문의</Text>
 
 						<TouchableOpacity activeOpacity={0.9}
-										  onPress={() => navigation.navigate('GuideListPage', { title:'윌라소개 & 이용가이드'} )}>
+										  onPress={() => navigation.navigate('GuideListPage', {title: '윌라소개 & 이용가이드'})}>
 							<View style={styles.guideItem}>
 								<Text style={styles.guideTitle}>윌라소개 & 이용가이드</Text>
 							</View>
 						</TouchableOpacity>
 						<View style={styles.guideHr}/>
 						<TouchableOpacity activeOpacity={0.9}
-										  onPress={() => navigation.navigate('InquireListPage', { title:'1:1 문의'} )}>
+										  onPress={() => navigation.navigate('InquireListPage', {title: '1:1 문의'})}>
 							<View style={styles.guideItem}>
 								<Text style={styles.guideTitle}>1:1 문의</Text>
 							</View>
