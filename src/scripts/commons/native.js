ece2783527671883f6f414bc333f4e1b3cea84dc
Pyunghyuk Yoo <yoophi@gmail.com>
2018-08-31 17:27:56 +0900

    native play support

--- src/scripts/commons/native.js
+++ src/scripts/commons/native.js
@@ -1,34 +1,42 @@
-import React from 'react';
-import {NativeModules} from 'react-native';
+import React from "react";
+import {NativeModules} from "react-native";
+
+const {RNNativePlayer} = NativeModules;
+import store from '../commons/store';
 
 
-const { RNNativePlayer } = NativeModules;
-
+import {Alert} from 'react-native'
 
 export default {
+	play(cid, oid = '') {
 
+		let userId = store.welaaaAuth.profile.id;
+		let accessToken = store.welaaaAuth.access_token;
 
-	play( id ){
 		/* TODO: id를 이용하여 api에서 필요 정보 받아오는 과정 필요 */
 
+		if (!userId) {
+			Alert.alert('비회원은 플레이할 수 없습니다.')
+
+			return true
+		}
+
 		let args = {
-			type: "streaming",
-			uri: "https://contents.welaaa.com/media/v100015/DASH_v100015_001/stream.mpd",
-			name: "지기지피 백전백승! 나의 발표 목적을 제일 먼저 고려하라",
-			drmSchemeUuid: "widevine",
-			drmLicenseUrl: "http://tokyo.pallycon.com/ri/licenseManager.do",
-			userId: "93", // 825  , 
-			cid: "v100015_001",
-			oid: "",
-			token: "", 
-			webToken: "" 
+			uri: '',
+			name: '',
+			drmSchemeUuid: 'widevine',
+			drmLicenseUrl: '',
+
+			cid: cid,
+			oid: oid,
+			userId: "" + userId, // 825  ,
+			token: accessToken // bearer token
 		};
 
+		console.log('native.play()', args)
 
-		setTimeout( ()=>{
-			RNNativePlayer.play( args );
-		}, 100 );
-
+		setTimeout(() => {
+			RNNativePlayer.play(args);
+		}, 100);
 	}
-
-}
+};
