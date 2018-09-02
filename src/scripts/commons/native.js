454b0c2dac9d3414a68f8092d521d33478d8b671
KyeJune Hong <hohoyaing@gmail.com>
2018-09-02 15:13:04 +0900

    데모 args에 uri및 name추가

--- src/scripts/commons/native.js
+++ src/scripts/commons/native.js
@@ -24,8 +24,8 @@ export default {
 		}
 
 		let args = {
-			uri: '',
-			name: '',
+			uri: 'https://contents.welaaa.com/media/v100015/DASH_v100015_001/stream.mpd',
+			name: '지기지피 백전백승! 나의 발표 목적을 제일 먼저 고려하라',
 			drmSchemeUuid: 'widevine',
 			drmLicenseUrl: 'http://tokyo.pallycon.com/ri/licenseManager.do',
 
@@ -35,11 +35,15 @@ export default {
 			token: accessToken // bearer token
 		};
 
-		console.log('native.play()', args)
+		console.log('native.play()', JSON.stringify(args));
 
 		setTimeout(() => {
-			RNNativePlayer.play(args);
 
+			try{
+				RNNativePlayer.play(args);
+			}catch (e) {
+				alert( e );
+			}
 
 			this.toggleMiniPlayer(true);
 		}, 100);
