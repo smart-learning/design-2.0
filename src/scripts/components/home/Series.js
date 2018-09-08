41268e5943169b7b5f06e28cee53d0e047443d6f
nanuRuya <uru@grotesq.com>
2018-09-08 17:59:31 +0900

    메인 > 추천시리즈: 데이터 표시방식 변경

--- src/scripts/components/home/Series.js
+++ src/scripts/components/home/Series.js
@@ -1,8 +1,8 @@
 import React from "react";
-import {Text, View, StyleSheet, TouchableOpacity, ImageBackground, Alert,} from "react-native";
-import CommonStyles from "../../../styles/common";
+import {Text, View, StyleSheet, TouchableOpacity, ImageBackground,} from "react-native";
+import {withNavigation} from "react-navigation";
 
-const styles = StyleSheet.create( {
+const styles = StyleSheet.create({
 	seriesItemSm: {
 		width: '48%',
 		marginBottom: 13,
@@ -29,106 +29,33 @@ const styles = StyleSheet.create( {
 		fontSize: 24,
 		color: '#ffffff',
 	},
-} );
+});
 
-export default class Series extends React.Component {
-	constructor( props ) {
-		super( props );
+export default withNavigation( class Series extends React.Component {
+	constructor(props) {
+		super(props);
 	}
 
 	render() {
 		return <View>
-			<View style={styles.seriesItemLg}>
-				<TouchableOpacity activeOpacity={0.9} onPress={() => {Alert.alert('준비중입니다.')}}>
-					<ImageBackground
-						source={{ uri: this.props.itemData[ 0 ].image }}
-						resizeMode="cover"
-						borderRadius={5}
-						style={styles.thumbnail}>
-						<Text style={styles.title}>
-							{this.props.itemData[ 0 ].title}
-						</Text>
-					</ImageBackground>
-				</TouchableOpacity>
-			</View>
-			<View style={styles.seriesItemLg}>
-				<TouchableOpacity activeOpacity={0.9} onPress={() => {Alert.alert('준비중입니다.')}}>
-					<ImageBackground
-						source={{ uri: this.props.itemData[ 1 ].image }}
-						resizeMode="cover"
-						borderRadius={5}
-						style={styles.thumbnail}>
-						<Text style={styles.title}>
-							{this.props.itemData[ 1 ].title}
-						</Text>
-					</ImageBackground>
-				</TouchableOpacity>
-			</View>
-			<View style={styles.seriesItemLg}>
-				<TouchableOpacity activeOpacity={0.9} onPress={() => {Alert.alert('준비중입니다.')}}>
-					<ImageBackground
-						source={{ uri: this.props.itemData[ 2 ].image }}
-						resizeMode="cover"
-						borderRadius={5}
-						style={styles.thumbnail}>
-						<Text style={styles.title}>
-							{this.props.itemData[ 2 ].title}
-						</Text>
-					</ImageBackground>
-				</TouchableOpacity>
-			</View>
-			<View style={styles.seriesItemLg}>
-				<TouchableOpacity activeOpacity={0.9} onPress={() => {Alert.alert('준비중입니다.')}}>
-					<ImageBackground
-						source={{ uri: this.props.itemData[ 3 ].image }}
-						resizeMode="cover"
-						borderRadius={5}
-						style={styles.thumbnail}>
-						<Text style={styles.title}>
-							{this.props.itemData[ 3 ].title}
-						</Text>
-					</ImageBackground>
-				</TouchableOpacity>
-			</View>
-			<View style={styles.seriesItemLg}>
-				<TouchableOpacity activeOpacity={0.9} onPress={() => {Alert.alert('준비중입니다.')}}>
-					<ImageBackground
-						source={{ uri: this.props.itemData[ 4 ].image }}
-						resizeMode="cover"
-						borderRadius={5}
-						style={styles.thumbnail}>
-						<Text style={styles.title}>
-							{this.props.itemData[ 4 ].title}
-						</Text>
-					</ImageBackground>
-				</TouchableOpacity>
-			</View>
-			<View style={styles.seriesItemLg} onPress={() => {Alert.alert('준비중입니다.')}}>
-				<TouchableOpacity activeOpacity={0.9}>
-					<ImageBackground
-						source={{ uri: this.props.itemData[ 5 ].image }}
-						resizeMode="cover"
-						borderRadius={5}
-						style={styles.thumbnail}>
-						<Text style={styles.title}>
-							{this.props.itemData[ 5 ].title}
-						</Text>
-					</ImageBackground>
-				</TouchableOpacity>
-			</View>
-			<View style={styles.seriesItemLg} onPress={() => {Alert.alert('준비중입니다.')}}>
-				<TouchableOpacity activeOpacity={0.9}>
-					<ImageBackground
-						source={{ uri: this.props.itemData[ 6 ].image }}
-						resizeMode="cover"
-						borderRadius={5}
-						style={styles.thumbnail}>
-						<Text style={styles.title}>
-							{this.props.itemData[ 6 ].title}
-						</Text>
-					</ImageBackground>
-				</TouchableOpacity>
-			</View>
+			{this.props.itemData.map((item, key) => {
+				return (
+					<View style={styles.seriesItemLg} key={key}>
+						<TouchableOpacity activeOpacity={0.9} onPress={ () => this.props.navigation.navigate('HomeSeriesPage', {id: '', title: ''})}>
+							<ImageBackground
+								source={{uri: item.image}}
+								resizeMode="cover"
+								borderRadius={5}
+								style={styles.thumbnail}>
+								<Text style={styles.title}>
+									{item.title}
+								</Text>
+							</ImageBackground>
+						</TouchableOpacity>
+					</View>
+				)
+			})}
+
 		</View>
 	}
-}
+} )
