054341de606f273a849b7db96e01c7bb99120cdf
TaeHyun <taehyun.kim@influential.co.kr>
2018-09-09 13:54:53 +0900

    F_Token을 RN으로 전달할 클래스 및 메서드 생성.

---
+++ ios/WelaaaV2/RNNativeBase.m
@@ -0,0 +1,34 @@
+
+#import "RNNativeBase.h"
+
+@implementation RNNativeBase
+
+RCT_EXPORT_MODULE();
+
+#pragma mark - Private Method
+
+- (NSString *) getWebToken
+{
+    // 1.0 로그인 유저의 F_Token을 확인합니다.
+    NSString *webToken = [[NSUserDefaults standardUserDefaults] stringForKey : @"webToken"];
+  
+    if ( nil == webToken )
+    {
+        webToken = @"NO_F_TOKEN";
+    }
+  
+    return webToken;
+}
+
+#pragma mark - RCT_EXPORT
+//
+// Obj-C -> JS callback method
+// 참고 : https://gist.github.com/chourobin/f83f3b3a6fd2053fad29fff69524f91c#file-callbacks-md
+//
+RCT_EXPORT_METHOD( getF_TOKEN : (RCTResponseSenderBlock) callback )
+{
+    NSString *f_token = [self getWebToken];
+    callback(@[[NSNull null], f_token]); // (error, someData) in js
+}
+
+@end
