16f0b973c6b5208c92fc8c79c069064c98d8c117
nanuRuya <uru@grotesq.com>
2018-09-16 18:31:33 +0900

    #174 매일책한권: 컨텐츠 터치범위 수정

--- src/scripts/components/home/BookDailyListItem.js
+++ src/scripts/components/home/BookDailyListItem.js
@@ -1,80 +1,94 @@
-import React from "react";
-import Summary from "../../components/video/Summary";
-import {
-	View,
-	Text,
-	StyleSheet
-} from "react-native";
-import m from "moment";
-import {COLOR_PRIMARY} from "../../../styles/common";
+import React from 'react';
+import Summary from '../../components/video/Summary';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import m from 'moment';
+import { COLOR_PRIMARY } from '../../../styles/common';
+import Native from '../../commons/native';
 
 const styles = StyleSheet.create({
-	bookListItemContainer: {
-		padding: 13,
-		borderStyle: 'solid',
-		borderWidth: 1,
-		borderColor: '#dddddd',
-	},
-	bookListItemHeadline: {
-		marginBottom: 13,
-	},
-	dateBox: {
-		position: 'relative',
-		flexDirection: 'column',
-		marginTop: 0,
-		marginBottom: 0,
-		marginRight: 'auto',
-		marginLeft: 'auto',
-	},
-	date: {
-		width: 50,
-		textAlign: 'center',
-		fontSize: 15,
-		color: COLOR_PRIMARY,
-	},
-	dateHr: {
-		width: 50,
-		height: 1,
-		backgroundColor: COLOR_PRIMARY,
-	},
-	listItemTitle: {
-		fontSize: 16,
-		color: '#333333',
-		marginTop: 14,
-	},
+  bookListItemContainer: {
+    padding: 13,
+    borderStyle: 'solid',
+    borderWidth: 1,
+    borderColor: '#dddddd'
+  },
+  bookListItemHeadline: {
+    marginBottom: 13
+  },
+  dateBox: {
+    position: 'relative',
+    flexDirection: 'column',
+    marginTop: 0,
+    marginBottom: 0,
+    marginRight: 'auto',
+    marginLeft: 'auto'
+  },
+  date: {
+    width: 50,
+    textAlign: 'center',
+    fontSize: 15,
+    color: COLOR_PRIMARY
+  },
+  dateHr: {
+    width: 50,
+    height: 1,
+    backgroundColor: COLOR_PRIMARY
+  },
+  listItemTitle: {
+    fontSize: 16,
+    color: '#333333',
+    marginTop: 14
+  }
 });
 
 class BookDailyListItem extends React.Component {
-	render() {
-		let itemData = {};
-		let month;
-		let day;
-		try {
-			itemData = this.props.itemData[0];
+  render() {
+    let itemData = {};
+    let month;
+    let day;
+    try {
+      itemData = this.props.itemData[0];
 
-			month = m( itemData.open_date ).format( "M" );
-			day = m( itemData.open_date ).format( "D" );
-		}
-		catch( error ) { console.log( error ) }
+      month = m(itemData.open_date).format('M');
+      day = m(itemData.open_date).format('D');
+    } catch (error) {
+      console.log(error);
+    }
 
-		return <View style={styles.bookListItemContainer}>
-			<View style={styles.bookListItemHeadline}>
-				<View style={styles.dateBox}>
-					<Text style={styles.date}>{ month } / { day }</Text>
-					<View style={styles.dateHr}/>
-				</View>
-				<Text style={styles.listItemTitle}>{ itemData ? itemData.title : '' }</Text>
-			</View>
-			<View>
-				<Summary type={ 'dailyBook' }
-						 thumbnail={ itemData ? itemData.image : 'bookDummy' }
-						 cid={ itemData ? itemData.cid : '' }
-						 hitCount={ itemData ? itemData.hit_count : '' }
-						 starAvg={ itemData ? itemData.star_avg : '' }
-						 reviewCount={ itemData ? itemData.review_count : '' }/>
-			</View>
-		</View>
-	}
+    console.log( 'dayily', itemData );
+
+    return (
+      <TouchableOpacity
+        activeOpacity={0.9}
+		onPress={() => Native.play(itemData.cid)}
+      >
+        <View style={styles.bookListItemContainer}>
+          <View style={styles.bookListItemHeadline}>
+            <View style={styles.dateBox}>
+              <Text style={styles.date}>
+                {month} / {day}
+              </Text>
+              <View style={styles.dateHr} />
+            </View>
+            <Text style={styles.listItemTitle}>
+              {itemData ? itemData.title : ''}
+            </Text>
+          </View>
+          <View>
+            <Summary
+              type={'dailyBook'}
+			  itemData={itemData}
+              thumbnail={itemData ? itemData.image : 'bookDummy'}
+              cid={itemData ? itemData.cid : ''}
+              hitCount={itemData ? itemData.hit_count : ''}
+              starAvg={itemData ? itemData.star_avg : ''}
+              reviewCount={itemData ? itemData.review_count : ''}
+            />
+          </View>
+        </View>
+      </TouchableOpacity>
+    );
+  }
 }
 
 export default BookDailyListItem;
