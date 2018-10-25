6603553771a0d846e365cdbed2b45f562410a8fa
TaeHyun <taehyun.kim@influential.co.kr>
2018-10-25 16:00:15 +0900

    인앱결제 관련 레거시 코드 제거 및 로직 개선.

--- ios/WelaaaV2/Payments/RNProductPayment.m
+++ ios/WelaaaV2/Payments/RNProductPayment.m
@@ -109,140 +109,128 @@ RCT_EXPORT_MODULE();
                     {
                       // 아래의 네비게이션 테스트를 위해 임시로 막아둠(response 콜백 보낼 때 적절한 json 값 셋팅 필요) 2018.10.24
                         // Convert JSON String to NSDictionary
-                      /*
                         NSDictionary *rec = [IAPShare toJSON : response];
-                        NSLog(@"  [IAP checkReceipt] response from the server : %@", response);
+                      //NSLog(@"  [IAP checkReceipt] response from the server : %@", response);
                         NSLog(@"  [IAP checkReceipt] rec from the server : %@", rec[@"status"]);
                
                         // status가 0이면 정상결제 처리된 것임.
                         if ( [rec[@"status"] integerValue] == 0 && nil != rec[@"status"] )
                         {
-                            [[IAPShare sharedHelper].iap provideContentWithTransaction : transaction];
-                 
+                            [[IAPShare sharedHelper].iap provideContentWithTransaction : transaction];  // 실효성이 있을까?
                             NSLog(@"  [IAP checkReceipt] SUCCESS IAP : %@", [IAPShare sharedHelper].iap.purchasedProducts);
                           
-                          //DEFAULT_ALERT(@"윌라", @"결제가 정상적으로 완료되었습니다.");
-                            [common presentAlertWithTitle:@"윌라" andMessage:@"결제가 정상적으로 완료되었습니다."];
+                            // 상품 구입 완료 후 RN에게 결제 결과를 전달해야 합니다.
+                            if ( [productCode hasPrefix : @"audiobook_"] )
+                            {
+                                NSLog(@"  [IAP checkReceipt] Done buying audiobook.");
+                              /*
+                                NSLog(@"RN 쪽에 홈화면으로 이동 요청");
+                                NSMutableDictionary *params = [[NSMutableDictionary alloc] init];
+                                [params setObject : [NSNumber numberWithBool:true]
+                                           forKey : @"success"];
+                                if ( self->_hasListeners )
+                                {
+                                    [self sendEventWithName : @"buyResult"
+                                                       body : params];
+                                }
+                              */
+                                [IAPShare sharedHelper].iap = nil;
+                              
+                                return ;
+                            }
+                            else if ( [productCode hasPrefix : @"m_"] )
+                            {
+                                NSLog(@"  [IAP checkReceipt] Done buying membership.");
+                              
+                                NSLog(@"RN 쪽에 홈화면으로 이동 요청");
+                                NSMutableDictionary *params = [[NSMutableDictionary alloc] init];
+                                [params setObject : [NSNumber numberWithBool:true]
+                                           forKey : @"success"];
+                                if ( self->_hasListeners )
+                                {
+                                    [self sendEventWithName : @"buyResult"
+                                                       body : params];
+                                }
+                              
+                                [IAPShare sharedHelper].iap = nil;
+                              
+                                return ;
+                            }
                         }
                         else
                         {
-                            //DEFAULT_ALERT(@"결제확인", @"트랜잭션은 정상이었지만 영수증 확인이 제대로 이루어지지 않았습니다..");
                             // 해당 상태를 NSUserDefaults로 저장하여 다음 앱 구동시에 해당 값을 읽어서 서버로 receipt verification을 한번 더 시도해야 합니다.
                             NSLog(@"  [IAP checkReceipt] Transaction was well done, but receipt validation has problem..");
+                            [IAPShare sharedHelper].iap = nil;
+                          
+                            return [common presentAlertWithTitle:@"윌라" andMessage:@"상품 준비중에 문제가 발생되었습니다.\n1:1문의를 남겨주세요."];
                         }
                
                         NSLog(@"  [IAP checkReceipt] 영수증 확인 후 다음 결제를 위해 PaymentQueue를 초기화합니다..");
                         [IAPShare sharedHelper].iap = nil;
-                      */
-                      
-                      // 2018.10.24
-                      NSLog(@"RN 쪽에 홈화면으로 이동 요청");
-                      NSMutableDictionary *params = [[NSMutableDictionary alloc] init];
-                      [params setObject:[NSNumber numberWithBool:true] forKey:@"success"];
-                      if ( self->_hasListeners )
-                      {
-                        [self sendEventWithName : @"buyResult"
-                                           body : params];
-                      }
-                      
                     }];
-                    // ios dispatchqueue
-                    // https://magi82.github.io/gcd-01/
-                    // http://jdub7138.blog.me/220949191761
-                    //
-                    // dispatchqueue.main.sync objective c
-                    // https://stackoverflow.com/questions/5662360/gcd-to-perform-task-in-main-thread
-                    //
-                    // runOnMainQueueWithoutDeadlocking
-                    // http://code.i-harness.com/ko-kr/q/566698
-                    //
                     // 영수증 검증을 마쳤습니다.
                   
-                    // 상품 구입 완료 후 RN에게 결제 결과를 전달해야 합니다.
-                    // Obj-c -> JS
-                    // https://gist.github.com/chourobin/f83f3b3a6fd2053fad29fff69524f91c#file-events-md
-                    // event emit
-                    if ( [productCode hasPrefix : @"audiobook_"] )
-                    {
-                      //RNReceiptEventEmitter *notification = [RNReceiptEventEmitter allocWithZone : nil];
-                      //[notification sendPaymentResultToReactNative];
-                        NSLog(@"  [IAP checkReceipt] Done buying audiobook.");
-                    }
-                    else if ( [productCode hasPrefix : @"m_"] )
-                    {
-                        NSLog(@"  [IAP checkReceipt] Done buying membership.");
-                    }
-            
                     [IAPShare sharedHelper].iap = nil;  // PaymentQueue를 완료시킵니다.
                 }
                 else if ( transaction.error.code == SKErrorPaymentCancelled )
                 {
                     [IAPShare sharedHelper].iap = nil;
             
-                  //return DEFAULT_ALERT(@"윌라", @"결제가 취소되었습니다.\n다음에 다시 이용해 주십시오.");
                     return [common presentAlertWithTitle:@"윌라" andMessage:@"결제가 취소되었습니다.\n다음에 다시 이용해 주십시오."];
                 }
                 else if ( transaction.error.code == SKErrorClientInvalid )
                 {
                     [IAPShare sharedHelper].iap = nil;
                   
-                  //return DEFAULT_ALERT(@"윌라", @"현재 고객님의 결제를 수행할 수 었습니다.\n다음에 다시 이용해 주십시오.");
                     return [common presentAlertWithTitle:@"윌라" andMessage:@"현재 고객님의 결제를 수행할 수 었습니다.\n다음에 다시 이용해 주십시오."];
                 }
                 else if ( transaction.error.code == SKErrorPaymentInvalid )
                 {
                     [IAPShare sharedHelper].iap = nil;
                   
-                  //return DEFAULT_ALERT(@"윌라", @"고객님의 지불수단이 App Store에서 인식되지 않았습니다.\n다음에 다시 이용해 주십시오.");
                     return [common presentAlertWithTitle:@"윌라" andMessage:@"고객님의 지불수단이 App Store에서 인식되지 않았습니다.\n다음에 다시 이용해 주십시오."];
                 }
                 else if ( transaction.error.code == SKErrorPaymentNotAllowed )
                 {
                     [IAPShare sharedHelper].iap = nil;
                   
-                  //return DEFAULT_ALERT(@"윌라", @"고객님의 결제는 승인되지 않았습니다.\n다음에 다시 이용해 주십시오.");
                     return [common presentAlertWithTitle:@"윌라" andMessage:@"고객님의 결제는 승인되지 않았습니다.\n다음에 다시 이용해 주십시오."];
                 }
                 else if ( transaction.error.code == SKErrorStoreProductNotAvailable )
                 {
                     [IAPShare sharedHelper].iap = nil;
                   
-                  //return DEFAULT_ALERT(@"윌라", @"요청하신 상품을 현재 App Store에서 사용할 수 없습니다.\n다음에 다시 이용해 주십시오.");
                     return [common presentAlertWithTitle:@"윌라" andMessage:@"요청하신 상품을 현재 App Store에서 사용할 수 없습니다.\n다음에 다시 이용해 주십시오."];
                 }
                 else if ( transaction.error.code == SKErrorCloudServicePermissionDenied )
                 {
                     [IAPShare sharedHelper].iap = nil;
                   
-                  //return DEFAULT_ALERT(@"윌라", @"고객님께서 클라우드 서비스 정보에 대한 접근을 허용하지 않았습니다.\n다음에 다시 이용해 주십시오.");
                     return [common presentAlertWithTitle:@"윌라" andMessage:@"고객님께서 클라우드 서비스 정보에 대한 접근을 허용하지 않았습니다.\n다음에 다시 이용해 주십시오."];
                 }
                 else if ( transaction.error.code == SKErrorCloudServiceNetworkConnectionFailed )
                 {
                     [IAPShare sharedHelper].iap = nil;
                   
-                  //return DEFAULT_ALERT(@"윌라", @"네트워크에 연결할 수 없습니다.\n다음에 다시 이용해 주십시오.");
                     return [common presentAlertWithTitle:@"윌라" andMessage:@"네트워크에 연결할 수 없습니다.\n다음에 다시 이용해 주십시오."];
                 }
                 else if ( transaction.error.code == SKErrorCloudServiceRevoked )
                 {
                     [IAPShare sharedHelper].iap = nil;
                   
-                  //return DEFAULT_ALERT(@"윌라", @"고객님께서 클라우드 서비스 사용 권한을 취소했습니다.\n다음에 다시 이용해 주십시오.");
                     return [common presentAlertWithTitle:@"윌라" andMessage:@"고객님께서 클라우드 서비스 사용 권한을 취소했습니다.\n다음에 다시 이용해 주십시오."];
                 }
                 else if ( transaction.error.code == SKErrorUnknown )
                 {
                     [IAPShare sharedHelper].iap = nil;
                   
-                  //return DEFAULT_ALERT(@"윌라", @"예기치 않은 오류가 발생했습니다.\n다음에 다시 이용해 주십시오.");
                     return [common presentAlertWithTitle:@"윌라" andMessage:@"예기치 않은 오류가 발생했습니다.\n다음에 다시 이용해 주십시오."];
                 }
                 else if ( transaction.transactionState == SKPaymentTransactionStateFailed )
                 {
                     [IAPShare sharedHelper].iap = nil;
                   
-                  //return DEFAULT_ALERT(@"윌라", @"iTunes Store에 접속할 수 없어 결제가 정상적으로 처리되지 않았습니다.\nSKPaymentTransactionStateFailed.");
                     return [common presentAlertWithTitle : @"윌라"
                                               andMessage : @"App Store에 접속할 수 없어 결제가 정상적으로 처리되지 않았습니다.\nSKPaymentTransactionStateFailed."];
                 }
@@ -252,7 +240,6 @@ RCT_EXPORT_MODULE();
                   
                     [IAPShare sharedHelper].iap = nil;
                   
-                  //return DEFAULT_ALERT(@"윌라", @"멤버십 구매복원을 처리 중입니다.........");
                     return [common presentAlertWithTitle:@"윌라" andMessage:@"멤버십 구매복원을 처리 중입니다........."];
                 }
             }]; // end of buying a product
