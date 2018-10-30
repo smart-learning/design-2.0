c5ed96409f4262139ac7bdb678fdadc6d4281103
jungon <jungon.kim@influential.co.kr>
2018-10-30 09:24:12 +0900

    console.log 제거

--- src/scripts/components/AdvertisingSection.js
+++ src/scripts/components/AdvertisingSection.js
@@ -1,193 +1,193 @@
 import React, { Component } from 'react';
-import { AsyncStorage, Button, Alert, Modal, StyleSheet, Dimensions, View, Text, TouchableOpacity } from "react-native";
+import {
+  AsyncStorage,
+  Button,
+  Alert,
+  Modal,
+  StyleSheet,
+  Dimensions,
+  View,
+  Text,
+  TouchableOpacity
+} from 'react-native';
 import Image from 'react-native-scalable-image';
-import { COLOR_PRIMARY } from "../../styles/common";
+import { COLOR_PRIMARY } from '../../styles/common';
 import moment from 'moment';
-import net from "../commons/net";
-import nav from "../commons/nav";
+import net from '../commons/net';
+import nav from '../commons/nav';
 import globalStore from '../commons/store';
 
 class AdvertisingSection extends Component {
+  constructor() {
+    super();
 
-	constructor() {
-		super();
+    this.style = StyleSheet.create({
+      container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#00000099'
+      },
 
-		this.style = StyleSheet.create({
+      frame: {
+        width: Dimensions.get('window').width - 30,
+        borderRadius: 20,
+        overflow: 'hidden',
+        backgroundColor: '#FFFFFF'
+      },
 
-			container: {
-				flex: 1,
-				alignItems: 'center',
-				justifyContent: 'center',
-				backgroundColor: '#00000099',
-			},
+      popupInfo: {
+        flex: 1,
+        position: 'absolute',
+        alignItems: 'center',
+        justifyContent: 'center'
+      },
 
-			frame: {
-				width: Dimensions.get('window').width - 30,
-				borderRadius: 20,
-				overflow: 'hidden',
-				backgroundColor: '#FFFFFF',
-			},
+      img: {
+        // borderTopRightRadius: 20,
+        // borderTopLeftRadius: 20,
+        // overflow: 'hidden',
+      },
 
-			popupInfo: {
-				flex:1,
-				position:'absolute',
-				alignItems:'center',
-				justifyContent:'center'
-			},
+      footer: {
+        backgroundColor: COLOR_PRIMARY,
+        flexDirection: 'row',
+        width: '100%',
+        alignItems: 'center'
+      },
 
-			img: {
-				// borderTopRightRadius: 20,
-				// borderTopLeftRadius: 20,
-				// overflow: 'hidden',
-			},
+      hideOption: {
+        padding: 10,
+        alignItems: 'flex-start'
+      },
 
-			footer: {
-				backgroundColor: COLOR_PRIMARY,
-				flexDirection: 'row',
-				width: '100%',
-				alignItems: 'center'
-			},
+      footerText: {
+        width: '100%',
+        padding: 15,
+        fontSize: 18,
+        color: '#FFFFFF',
+        textAlign: 'center'
+      }
+    });
 
-			hideOption: {
-				padding: 10,
-				alignItems: 'flex-start',
-			},
+    this.state = {
+      ads: []
+    };
 
-			footerText: {
-				width: '100%',
-				padding: 15,
-				fontSize: 18,
-				color: '#FFFFFF',
-				textAlign: 'center'
-			}
-		});
+    this.now = moment();
+  }
 
+  componentDidMount = async () => {
+    let data = await net.getMainPopup();
+    if (data.length === 0) return;
 
-		this.state = {
-			ads: [],
-		}
+    //메인화면이 아닐경우 팝업 뜨지 않게 설정
+    //console.log(globalStore);
+    if (globalStore.lastLocation != 'HomeScreen') {
+      return;
+    }
 
-		this.now = moment();
-	}
+    // 안보기로 한 팝업은 아닌지 날짜 확인
+    const adsIds = data.map(item => `pop-${item.id}`);
+    const adKeyDateMaps = await AsyncStorage.multiGet(adsIds);
 
-	componentDidMount = async () => {
-		let data = await net.getMainPopup();
-		if (data.length === 0) return;
+    // 설정된적이 없거나 날짜가 남았다면 ad리스트에 추가
+    let ads = [];
+    data.forEach((ad, idx) => {
+      let expireDate = adKeyDateMaps[idx][1];
+      if (expireDate === null) {
+        ads.push(ad);
+      } else {
+        let fromNowDays = this.now.diff(moment(expireDate), 'days');
+        if (fromNowDays > 0) ads.push(ad);
+      }
+    });
 
-		//메인화면이 아닐경우 팝업 뜨지 않게 설정
-		console.log(globalStore);
-		if (globalStore.lastLocation != 'HomeScreen'){
-			return;
-		}
+    this.setState({ ads: ads });
+  };
 
+  onConfirm = () => {
+    let ads = [...this.state.ads];
+    let closedAd = ads.shift();
+    this.setState({ ads: ads });
 
-		// 안보기로 한 팝업은 아닌지 날짜 확인
-		const adsIds = data.map(item => `pop-${item.id}`);
-		const adKeyDateMaps = await AsyncStorage.multiGet(adsIds);
+    return closedAd;
+  };
 
+  onCancel = () => {
+    // this.setState({ modalId:null });
+  };
 
-		// 설정된적이 없거나 날짜가 남았다면 ad리스트에 추가
-		let ads = [];
-		data.forEach((ad, idx) => {
-			let expireDate = adKeyDateMaps[idx][1];
-			if (expireDate === null) {
-				ads.push(ad);
-			}
-			else {
-				let fromNowDays = this.now.diff(moment(expireDate), 'days');
-				if (fromNowDays > 0) ads.push(ad);
-			}
+  hide3Days = () => {
+    let closedAd = this.onConfirm();
 
-		});
+    //console.log(closedAd);
 
-		this.setState({ ads: ads });
-	}
+    const after3Days = moment()
+      .add(3, 'd')
+      .format()
+      .toString();
+    AsyncStorage.setItem(`pop-${closedAd.id}`, after3Days);
+  };
 
+  goEvent = info => {
+    nav.parseDeepLink('welaaa://' + info.action_type + '/' + info.action_param);
+  };
 
+  render() {
+    let ad = {};
+    let img_ratio = 1.0;
+    const cnt = this.state.ads.length;
+    const device_size = Dimensions.get('window');
 
-	onConfirm = () => {
-		let ads = [...this.state.ads];
-		let closedAd = ads.shift();
-		this.setState({ ads: ads });
+    if (cnt > 0) {
+      ad = this.state.ads[cnt - 1];
+      img_ratio = (device_size.width - 50) / ad.img_width;
+    }
 
-		return closedAd;
-	}
+    return (
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={cnt > 0}
+        onRequestClose={() => {}}
+      >
+        <View style={this.style.container}>
+          <View style={this.style.frame}>
+            <TouchableOpacity
+              activeOpacity={0.9}
+              style={this.style.hideOption}
+              onPress={() => this.goEvent(ad)}
+            >
+              <View style={[{ height: ad.img_height * img_ratio }]} />
+              <Image
+                source={{ uri: ad.img_url }}
+                width={device_size.width - 30}
+                style={this.style.popupInfo}
+                resizeMode={'cover'}
+              />
+            </TouchableOpacity>
 
-	onCancel = () => {
-		// this.setState({ modalId:null });
-	}
+            <TouchableOpacity
+              activeOpacity={0.9}
+              style={this.style.hideOption}
+              onPress={this.hide3Days}
+            >
+              <Text>3일동안 보지 않기</Text>
+            </TouchableOpacity>
 
-	hide3Days = () => {
-		let closedAd = this.onConfirm();
-
-		console.log(closedAd);
-
-		const after3Days = moment().add(3, 'd').format().toString();
-		AsyncStorage.setItem(`pop-${closedAd.id}`, after3Days);
-	}
-
-	goEvent = (info) => {
-
-		nav.parseDeepLink('welaaa://'+info.action_type+'/'+info.action_param);
-	}
-
-
-	render() {
-
-		let ad = {};
-		let img_ratio = 1.0;
-		const cnt = this.state.ads.length;
-		const device_size = Dimensions.get('window');
-		
-		if (cnt > 0){
-			ad = this.state.ads[cnt - 1];
-			img_ratio = (device_size.width-50)/ad.img_width;
-		}
-
-
-		return <Modal
-			animationType="slide"
-			transparent={true}
-			visible={cnt > 0}
-			onRequestClose={() => { }}
-		>
-
-			<View style={this.style.container}>
-
-				<View style={this.style.frame}>
-
-					<TouchableOpacity activeOpacity={0.9}
-						style={this.style.hideOption}
-						onPress={() => this.goEvent(ad)}
-					>
-						<View style={[ { height: ad.img_height*img_ratio} ]} ></View>
-						<Image source={{ uri: ad.img_url }}
-							width={device_size.width-30}
-							style={this.style.popupInfo}
-							resizeMode={'cover'}
-						/>
-					</TouchableOpacity>
-
-					<TouchableOpacity activeOpacity={0.9}
-						style={this.style.hideOption}
-						onPress={this.hide3Days}>
-						<Text>3일동안 보지 않기</Text>
-					</TouchableOpacity>
-
-					<TouchableOpacity activeOpacity={0.9}
-						style={this.style.footer}
-						onPress={this.onConfirm}>
-						<Text style={this.style.footerText}>닫기</Text>
-					</TouchableOpacity>
-
-				</View>
-
-			</View>
-
-		</Modal>
-	}
-
+            <TouchableOpacity
+              activeOpacity={0.9}
+              style={this.style.footer}
+              onPress={this.onConfirm}
+            >
+              <Text style={this.style.footerText}>닫기</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Modal>
+    );
+  }
 }
-
 
 export default AdvertisingSection;
