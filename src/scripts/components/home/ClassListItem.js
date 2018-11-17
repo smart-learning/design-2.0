157f5153f787de11872d680ebfd55c4362ee8509
Pyunghyuk Yoo <yoophi@gmail.com>
2018-11-17 17:49:23 +0900

    badge 맞추기
    뱃지가 누락되어 있는 부분을 채우는 작업을 하는 도중
    공통으로 쓰는 컴포넌트에 뱃지가 붙어있는 부분이 있어 뱃지가 중복으로
    나오는 문제를 해결함

--- src/scripts/components/home/ClassListItem.js
+++ src/scripts/components/home/ClassListItem.js
@@ -88,7 +88,6 @@ class ClassListItem extends React.Component {
       <View style={styles.classItem}>
         <TouchableOpacity activeOpacity={0.9} onPress={this.gotoClassPage}>
           <View>
-            <View style={[CommonStyles.alignJustifyItemCenter]}>
               {this.props.classType === 'hot' && (
                 <View style={styles.classRank}>
                   <Text style={styles.classRankText}>
@@ -97,43 +96,6 @@ class ClassListItem extends React.Component {
                   <View style={styles.classRankHr} />
                 </View>
               )}
-              <View>
-                <View style={CommonStyles.alignJustifyFlex}>
-                  {!!this.props.itemData.is_new && (
-                    <View
-                      style={[styles.classLabel, styles.classLabelNew]}
-                      borderRadius={10}
-                    >
-                      <Text style={styles.classLabelText}>NEW</Text>
-                    </View>
-                  )}
-                  {!!this.props.itemData.is_featured && (
-                    <View
-                      style={[styles.classLabel, styles.classLabelFeatured]}
-                      borderRadius={10}
-                    >
-                      <Text style={styles.classLabelText}>추천</Text>
-                    </View>
-                  )}
-                  {!!this.props.itemData.is_exclusive && (
-                    <View
-                      style={[styles.classLabel, styles.classLabelExclusive]}
-                      borderRadius={10}
-                    >
-                      <Text style={styles.classLabelText}>독점</Text>
-                    </View>
-                  )}
-                  {!!this.props.itemData.is_free && (
-                    <View
-                      style={[styles.classLabel, styles.classLabelFree]}
-                      borderRadius={10}
-                    >
-                      <Text style={styles.classLabelText}>무료</Text>
-                    </View>
-                  )}
-                </View>
-              </View>
-            </View>
             <Text style={styles.classTitle}>
               {this.props.itemData.headline}
             </Text>
