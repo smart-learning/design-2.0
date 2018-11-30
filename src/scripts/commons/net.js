926e78c5c3b994a6557ea322205c52d9986ba7be
TaeHyun <taehyun.kim@influential.co.kr>
2018-11-30 17:41:49 +0900

    메인배너 정책을 안드로이드 기준으로 통일.

--- src/scripts/commons/net.js
+++ src/scripts/commons/net.js
@@ -465,7 +465,7 @@ export default {
       expired = 1;
     }
     return cacheOrLoad(
-      API_PREFIX + 'v1.0/cms/main/banner?hide_ios=true',
+      API_PREFIX + 'v1.0/cms/main/banner?platform=' + Platform.OS,
       expired
     )
       .then(data => {
