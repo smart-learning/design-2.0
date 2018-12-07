788cba705e5bf9b813b8ba89d1d837dcbac94649
nanuRuya <uru@grotesq.com>
2018-12-07 17:54:45 +0900

    design-2.0-#89: 메인 > 인기클래스: 컴포넌트 레이아웃 변경

--- src/scripts/components/home/ClipRankItem.js
+++ src/scripts/components/home/ClipRankItem.js
@@ -1,83 +1,113 @@
-import React from "react";
-import {observable} from 'mobx';
-import {observer} from 'mobx-react';
-import {Text, View, StyleSheet, TouchableOpacity, ImageBackground, Dimensions, Image,} from "react-native";
-import CommonStyles from "../../../styles/common";
-import icChartStar from '../../../images/ic-chart-star.png';
-import icChartMessage from '../../../images/ic-chart-message.png';
+import React from 'react';
+import { observable } from 'mobx';
+import { observer } from 'mobx-react';
+import {
+  Text,
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  ImageBackground,
+  Dimensions,
+  Image
+} from 'react-native';
+import CommonStyles from '../../../styles/common';
+import IcPlay from '../../../images/ic-play-dark.png';
+import IcPlayBtn from '../../../images/ic-play-green.png';
+import numeral from 'numeral';
 
 const styles = StyleSheet.create({
-
-	rankGridItem: {
-		marginBottom: 15,
-	},
-	rankNumber: {
-		width: 25,
-	},
-	rankNumberText: {
-		fontSize: 16,
-		fontWeight: 'bold',
-		textAlign: 'center',
-		color: '#4a4a4a',
-	},
-	thumbnail: {
-		width: 40,
-		height: 40,
-		marginRight: 10,
-		marginLeft: 10,
-	},
-	rankIcon: {
-		position: 'relative',
-		top: 1,
-		width: 14,
-		height: 14,
-		marginRight: 2,
-	},
-	rankContentText: {
-		paddingRight: 14,
-		fontSize: 11,
-		color: '#888888',
-	},
+  rankGridItem: {
+    marginBottom: 10
+  },
+  rankNumber: {
+    width: 25
+  },
+  rankNumberText: {
+    fontSize: 14,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: '#353A3C'
+  },
+  thumbnail: {
+    width: 60,
+    height: 60,
+    marginRight: 10,
+    marginLeft: 10
+  },
+  rankIcon: {
+    position: 'relative',
+    top: 1,
+    width: 14,
+    height: 14,
+    marginRight: 2
+  },
+  rankContentText: {
+    paddingRight: 20,
+    fontSize: 12,
+    color: '#767B80'
+  },
+  contentHr: {
+    height: 1,
+    backgroundColor: '#E2E2E2',
+    marginBottom: 10
+  },
+  rankTeacherText: {
+    width: 95,
+    paddingRight: 20,
+    fontSize: 12,
+    color: '#767B80'
+  },
+  playButton: {
+    width: 30,
+    height: 30,
+    marginLeft: 'auto'
+  }
 });
 
 export default class ClipRankItem extends React.Component {
-	render() {
-		return <View style={styles.rankGridItem}>
-			<View style={CommonStyles.alignJustifyItemCenter}>
-				<View style={styles.rankNumber}>
-					<Text style={styles.rankNumberText}>
-						{this.props.itemData.rankNumber}
-					</Text>
-				</View>
-				<ImageBackground
-					source={{uri: this.props.itemData.images.list}}
-					resizeMode="cover"
-					style={styles.thumbnail}/>
-				<View style={{width: this.props.itemData.clipRankContentSize}}>
-					<TouchableOpacity>
-						<Text>
-							{this.props.itemData.title}
-						</Text>
-					</TouchableOpacity>
-					<View style={CommonStyles.alignJustifyFlex}>
-						{1 === 2 &&
-						<View>
-							<Image source={icChartStar} style={styles.rankIcon}/>
-							<Text style={styles.rankContentText}>
-								{this.props.itemData.star_avg.toFixed(1)}
-							</Text>
-							<Image source={icChartMessage} style={styles.rankIcon}/>
-							<Text style={styles.rankContentText}>
-								{this.props.itemData.review_count}
-							</Text>
-						</View>
-						}
-						<Text style={styles.rankContentText}>
-							{this.props.itemData.course.teacher.name}
-						</Text>
-					</View>
-				</View>
-			</View>
-		</View>
-	}
+  render() {
+    return (
+      <View style={styles.rankGridItem}>
+        <View style={styles.contentHr} />
+        <View style={CommonStyles.alignJustifyItemCenter}>
+          <View style={styles.rankNumber}>
+            <Text style={styles.rankNumberText}>
+              {this.props.itemData.rankNumber}
+            </Text>
+          </View>
+          <ImageBackground
+            source={{ uri: this.props.itemData.images.list }}
+            resizeMode="cover"
+            style={styles.thumbnail}
+            borderRadius={4}
+          />
+          <View style={{ width: '51%' }}>
+            <TouchableOpacity>
+              <Text ellipsizeMode={'tail'} numberOfLines={2}>
+                {this.props.itemData.title}
+              </Text>
+            </TouchableOpacity>
+            <View style={CommonStyles.alignJustifyFlex}>
+              <Image source={IcPlay} style={styles.rankIcon} />
+              <Text style={styles.rankContentText}>
+                {numeral(
+                  this.props.itemData.meta
+                    ? this.props.itemData.meta.play_count
+                    : this.props.itemData.hit_count
+                ).format('0a')}
+              </Text>
+              <Text
+                style={styles.rankTeacherText}
+                ellipsizeMode={'tail'}
+                numberOfLines={1}
+              >
+                {this.props.itemData.course.teacher.name}
+              </Text>
+            </View>
+          </View>
+          <Image source={IcPlayBtn} style={styles.playButton} />
+        </View>
+      </View>
+    );
+  }
 }
