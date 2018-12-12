f9244dcdd93d3000e19459e9f0c778907124715c
hanvic <victoryhan@gmail.com>
2018-12-12 16:55:43 +0900

    2018년 12월 이달의 책 김소영 전 아나운서 소개 이미지 추가

--- src/scripts/components/home/BookMonthlyItem.js
+++ src/scripts/components/home/BookMonthlyItem.js
@@ -1,28 +1,26 @@
 import React from 'react';
-import { ImageBackground, StyleSheet, Text, View } from 'react-native';
+import { ImageBackground, StyleSheet, Text, View, Image } from 'react-native';
 import Dummy from '../../../images/dummy-audioBook.png';
 
 const styles = StyleSheet.create({
   bookItem: {
     flexDirection: 'row',
     alignItems: 'center',
-    // width: 250,
     width: '100%',
-    height: 160,
+    height: 200,
     marginLeft: 'auto',
     marginRight: 'auto',
-    marginBottom: 20
   },
   title: {
     fontSize: 16,
     fontWeight: 'bold',
-    color: '#333333'
+    color: '#333333',
   },
   author: {
     paddingTop: 10,
     paddingBottom: 10,
     fontSize: 12,
-    color: '#555555'
+    color: '#555555',
   },
   detailButton: {
     justifyContent: 'center',
@@ -31,28 +29,34 @@ const styles = StyleSheet.create({
     paddingLeft: 10,
     paddingRight: 10,
     borderWidth: 1,
-    borderColor: '#555555'
+    borderColor: '#555555',
   },
   detailButtonText: {
     textAlign: 'center',
     fontSize: 12,
-    color: '#555555'
+    color: '#555555',
   },
 
   bookThumbnailWrap: {
     width: '40%',
-    height: 150
+    height: 150,
   },
-
   bookThumbnailContainer: {
     width: 98,
-    height: 143
+    height: 143,
   },
   thumbnail: {
     width: '100%',
     paddingTop: '80%',
-    paddingBottom: '80%'
-  }
+    paddingBottom: '80%',
+  },
+  botm_201812_A: {
+    position: 'absolute',
+    right: 0,
+    bottom: 0,
+    width: 140,
+    height: 80,
+  },
 });
 
 export default class BookMonthlyItem extends React.Component {
@@ -65,7 +69,7 @@ export default class BookMonthlyItem extends React.Component {
               position: 'absolute',
               right: 0,
               top: 0,
-              height: 150
+              height: 150,
             }}
           >
             <View style={styles.bookThumbnailContainer}>
@@ -74,7 +78,7 @@ export default class BookMonthlyItem extends React.Component {
                   width: 98,
                   height: 143,
                   paddingTop: '80%',
-                  paddingBottom: '80%'
+                  paddingBottom: '80%',
                 }}
                 source={
                   this.props.itemData.audiobook.images
@@ -92,7 +96,7 @@ export default class BookMonthlyItem extends React.Component {
           style={{
             width: '60%',
             paddingLeft: 16,
-            paddingRight: 40
+            paddingRight: 40,
           }}
         >
           <Text style={styles.title}>{this.props.itemData.title}</Text>
@@ -102,14 +106,24 @@ export default class BookMonthlyItem extends React.Component {
             style={[
               styles.detailButton,
               {
-                width: 120
-              }
+                width: 120,
+              },
             ]}
             borderRadius={13}
           >
             <Text style={styles.detailButtonText}>자세히보기</Text>
           </View>
         </View>
+        {/* 김소영 아나운서 이미지 출력 */}
+        {this.props.itemData.id === 22 && (
+          <Image
+            source={{
+              uri:
+                'https://static.welaaa.co.kr/static/botm/2018-12_botm_A_bg.png',
+            }}
+            style={styles.botm_201812_A}
+          />
+        )}
       </View>
     );
   }
