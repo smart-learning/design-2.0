74c1e65dc4bcd21d93fa1a6c2cf785d882a6d003
nanuRuya <uru@grotesq.com>
2019-01-04 14:37:21 +0900

    design-2.0-#19, #20, #21, #22: 상세페이지 공통] 컨텐츠 영역 표시수정

--- src/scripts/components/detail/TabContentInfo.js
+++ src/scripts/components/detail/TabContentInfo.js
@@ -9,7 +9,7 @@ import {
   StyleSheet,
   Text,
   TouchableOpacity,
-  View
+  View,
 } from 'react-native';
 import Swiper from 'react-native-swiper';
 import _ from 'underscore';
@@ -25,129 +25,140 @@ const styles = StyleSheet.create({
   slide: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
   },
   thumbnail: {
     width: '100%',
     paddingTop: '42%',
-    paddingBottom: '42%'
+    paddingBottom: '42%',
   },
   info: {
     paddingTop: 30,
-    paddingBottom: 30
+    paddingRight: 20,
+    paddingBottom: 30,
+    paddingLeft: 20,
   },
   sectionTitle: {
     marginBottom: 20,
     fontSize: 18,
     fontWeight: 'bold',
-    color: '#34342C'
+    color: '#34342C',
   },
   infoTextNormal: {
-    fontSize: 13,
+    fontSize: 12,
     lineHeight: 20,
-    color: '#555555'
+    color: '#353a3c',
   },
   lectureMoreButton: {
     textAlign: 'right',
     fontSize: 13,
-    color: CommonStyles.COLOR_PRIMARY
+    color: CommonStyles.COLOR_PRIMARY,
+  },
+  contentHrWrap: {
+    paddingLeft: 25,
+    paddingRight: 25,
   },
   contentHr: {
     width: '100%',
     height: 1,
-    backgroundColor: '#dddddd'
+    backgroundColor: '#dddddd',
   },
   labelInfo: {
-    paddingTop: 30,
+    paddingTop: 20,
+    paddingRight: 25,
     paddingBottom: 20,
-    paddingLeft: 30
+    paddingLeft: 25,
   },
   labelInfoItem: {
     alignItems: 'center',
-    marginBottom: 12
+    marginBottom: 7,
   },
   labelInfoImage: {
-    width: 24,
-    height: 25,
-    marginRight: 10
+    width: 12,
+    height: 12,
+    marginRight: 7,
   },
   labelInfoText: {
-    fontSize: 13,
-    color: '#888888'
+    fontSize: 12,
+    color: '#353A3C',
+  },
+  labelInfoSpanText: {
+    color: CommonStyles.COLOR_PRIMARY,
   },
   author: {
     paddingTop: 30,
     paddingBottom: 30,
-    paddingLeft: 15,
-    paddingRight: 15,
-    backgroundColor: '#f1f1f1'
+    paddingLeft: 25,
+    paddingRight: 25,
   },
   review: {
     paddingTop: 30,
     paddingRight: 15,
     paddingBottom: 30,
-    paddingLeft: 15
+    paddingLeft: 15,
   },
   teacherContainer: {
     width: '100%',
     padding: 25,
-    backgroundColor: '#ffffff'
+    backgroundColor: '#ffffff',
   },
   teacherThumbnail: {
     position: 'absolute',
     left: 0,
-    width: 72,
-    height: 72,
-    borderRadius: 35
+    width: 75,
+    height: 75,
+    borderRadius: 38,
+    borderWidth: 1,
+    borderColor: CommonStyles.COLOR_PRIMARY,
   },
   teacherTitle: {
     fontSize: 14,
     fontWeight: '200',
     color: '#4a4a4a',
-    marginBottom: 5
+    marginBottom: 5,
   },
   teacherHeadline: {
-    fontSize: 15,
+    fontSize: 13,
     fontWeight: 'normal',
-    color: '#34342C'
+    color: '#353A3C',
   },
   teacherName: {
+    paddingBottom: 10,
     fontSize: 16,
     fontWeight: 'bold',
-    color: '#34342C'
+    color: '#353A3C',
   },
-  teacherMemo: {
-    paddingTop: 25,
-    paddingBottom: 25
-  },
+  teacherMemo: {},
   teacherMemoText: {
-    color: '#000000',
+    paddingTop: 20,
+    paddingBottom: 25,
+    lineHeight: 20,
+    color: '#353A3C',
     fontWeight: '200',
-    fontSize: 14
+    fontSize: 12,
   },
   showMemoButtonText: {
     textAlign: 'right',
     fontSize: 14,
-    color: '#a1a1a1'
+    color: '#a1a1a1',
   },
   memoContainer: {
     width: '100%',
     padding: 25,
-    backgroundColor: '#EFEFEF'
   },
   memoTitle: {
     width: '100%',
     fontSize: 17,
     fontWeight: 'bold',
     marginBottom: 20,
-    color: '#4a4a4a'
+    color: '#4a4a4a',
   },
   memoBody: {
     width: '100%',
     fontSize: 15,
     fontWeight: '400',
     marginBottom: 25,
-    color: '#4a4a4a'
+    color: '#4a4a4a',
   },
   memoButton: {
     width: '100%',
@@ -157,13 +168,13 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     padding: 8,
     backgroundColor: COLOR_PRIMARY,
-    borderRadius: 7
+    borderRadius: 7,
   },
   memoButtonText: {
     fontSize: 15,
     textAlign: 'center',
-    color: '#ffffff'
-  }
+    color: '#ffffff',
+  },
 });
 
 class Data {
@@ -211,7 +222,7 @@ class TabContentInfo extends React.Component {
     // 	this.props.store.teacherView = false;
     // }
     this.setState({
-      teacherView: !this.state.teacherView
+      teacherView: !this.state.teacherView,
     });
   };
 
@@ -269,76 +280,85 @@ class TabContentInfo extends React.Component {
         )}
         {/* /이미지 스와이퍼 */}
 
-        <View style={[CommonStyles.contentContainer, styles.info]}>
-          <Text style={styles.sectionTitle}>기본정보</Text>
-
-          <Text style={styles.infoTextNormal}>
-            {this.props.store.itemData.title}
-          </Text>
-          <Text style={styles.infoTextNormal}>
-            {`${this.props.store.itemData.memo.split('<br>').join('\n')}`}
-          </Text>
-        </View>
-
-        <View style={styles.contentHr} />
-
         <View style={styles.labelInfo}>
           {this.props.learnType === 'class' && (
             <View style={[CommonStyles.alignJustifyFlex, styles.labelInfoItem]}>
               <Image style={styles.labelInfoImage} source={IcClip} />
               <Text style={styles.labelInfoText}>
-                {this.props.store.itemData.clip_count}개 강의 클립
+                <Text style={styles.labelInfoSpanText}>
+                  {this.props.store.itemData.clip_count}개
+                </Text>
+                의 강의
               </Text>
             </View>
           )}
           <View style={[CommonStyles.alignJustifyFlex, styles.labelInfoItem]}>
             <Image style={styles.labelInfoImage} source={IcTime} />
             <Text style={styles.labelInfoText}>
-              총 러닝타임 {`${playTime.hours()}시간 ${playTime.minutes()}분`}
+              전체 재생시간
+              <Text
+                style={styles.labelInfoSpanText}
+              >{`${playTime.hours()}시간 ${playTime.minutes()}분`}</Text>
             </Text>
           </View>
           <View style={[CommonStyles.alignJustifyFlex, styles.labelInfoItem]}>
             <Image style={styles.labelInfoImage} source={IcFile} />
             <Text style={styles.labelInfoText}>
-              콘텐츠 용량 {this.props.store.itemData.file_size} MB
+              다운로드 시 용량
+              <Text style={styles.labelInfoSpanText}>
+                {this.props.store.itemData.file_size} MB
+              </Text>
             </Text>
           </View>
-          {/*
-				<View style={[ CommonStyles.alignJustifyFlex, styles.labelInfoItem ]}>
-					<Image style={styles.labelInfoImage} source={IcPrize}/>
-					<Text style={styles.labelInfoText}>학습 완료 시 수료증 제공</Text>
-				</View>
-				*/}
+          {1 === 2 && (
+            <View style={[CommonStyles.alignJustifyFlex, styles.labelInfoItem]}>
+              <Image style={styles.labelInfoImage} source={IcPrize} />
+              <Text style={styles.labelInfoText}>학습 완료 시 수료증 제공</Text>
+            </View>
+          )}
+        </View>
+
+        <View style={styles.contentHrWrap}>
+          <View style={styles.contentHr} />
+        </View>
+
+        <View style={[CommonStyles.contentContainer, styles.info]}>
+          {1 === 2 && (
+            <Text style={styles.infoTextNormal}>
+              {this.props.store.itemData.title}
+            </Text>
+          )}
+
+          <Text style={styles.infoTextNormal}>
+            {`${this.props.store.itemData.memo.split('<br>').join('\n')}`}
+          </Text>
+        </View>
+
+        <View style={styles.contentHrWrap}>
+          <View style={styles.contentHr} />
         </View>
 
         <View style={styles.author}>
-          <View style={{ position: 'relative' }}>
+          <View style={{ position: 'relative', justifyContent: 'center' }}>
             {this.props.store.itemData.teacher !== null && (
               <Image
                 source={{
                   uri: this.props.store.itemData.teacher
                     ? this.props.store.itemData.teacher.images.default
-                    : DummyTeacher
+                    : DummyTeacher,
                 }}
                 style={styles.teacherThumbnail}
               />
             )}
 
             <View style={{ width: '100%', paddingLeft: 100 }}>
-              {this.props.learnType === 'audioBook' && (
-                <Text style={styles.teacherTitle}>저자</Text>
-              )}
-              {this.props.learnType === 'class' && (
-                <Text style={styles.teacherTitle}>강사</Text>
-              )}
-
               {this.props.store.itemData.teacher !== null && (
                 <View>
-                  <Text style={styles.teacherHeadline}>
-                    {this.props.store.itemData.teacher.headline}
-                  </Text>
                   <Text style={styles.teacherName}>
                     {this.props.store.itemData.teacher.name}
+                  </Text>
+                  <Text style={styles.teacherHeadline}>
+                    {this.props.store.itemData.teacher.headline}
                   </Text>
                 </View>
               )}
@@ -347,13 +367,6 @@ class TabContentInfo extends React.Component {
           <View style={styles.teacherMemo}>
             <Text style={styles.teacherMemoText}>{teacherMemo}</Text>
           </View>
-          {this.props.store.itemData.teacher !== null && (
-            <View style={{ width: '100%' }} />
-          )}
-
-          {/* {this.props.store.itemData.teacher.memo !== null && (
-            <View style={{ width: '100%' }} />
-          )} */}
         </View>
 
         {1 === 2 && (
