5a81484ec80b82f4aa1142f8bd4b66e3cbd1644e
nanuRuya <uru@grotesq.com>
2019-01-29 02:23:14 +0900

    design-2.0-#118: [클래스 > 상세 페이지] 구매정책 변경된 UI 적용

--- src/scripts/components/detail/VideoPaymentStatus.js
+++ src/scripts/components/detail/VideoPaymentStatus.js
@@ -13,113 +13,60 @@ import CommonStyles from '../../../styles/common';
 import moment from 'moment';
 
 const styles = StyleSheet.create({
-  paymentContainer: {
-    alignItems: 'center',
-    position: 'relative',
-    height: 45,
+  contentContainer: {
+    justifyContent: 'flex-end',
+    marginTop: 20,
+    paddingLeft: 25,
     paddingRight: 20,
-    paddingLeft: 20,
-    backgroundColor: '#ffffff',
   },
-  priceContainer: {
-    alignItems: 'center',
-  },
-  priceOriginal: {
-    paddingRight: 7,
-    fontSize: 20,
-    fontWeight: 'bold',
-    color: '#000000',
-  },
-  priceText: {
-    position: 'relative',
-    top: 2,
-    fontSize: 12,
-    color: '#000000',
-  },
-  rentDurationText: {
-    fontSize: 15,
-    fontWeight: 'bold',
-    color: '#000000',
-    paddingLeft: 7,
-  },
-  stateText: {
-    paddingRight: 7,
-    fontSize: 15,
-    fontWeight: 'bold',
-    color: '#000000',
-  },
-  priceDiscount: {
+  originPriceText: {
     textDecorationLine: 'line-through',
-    fontSize: 15,
-    fontWeight: 'bold',
+    fontSize: 17,
+    color: '#919191',
+  },
+  labelText: {
+    fontSize: 14,
+    color: '#919191',
+  },
+  paymentText: {
+    fontSize: 23,
+    fontWeight: '400',
     color: '#000000',
   },
-  buttonBuy: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    width: 80,
-    height: 30,
-    backgroundColor: CommonStyles.COLOR_PRIMARY,
+  durationText: {
+    fontSize: 13,
+    fontWeight: 'normal',
+    color: '#767B80',
   },
-  buttonBuyText: {
-    fontSize: 15,
-    fontWeight: 'bold',
-    color: '#ffffff',
-  },
-  buttonAdd: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    height: 40,
-    paddingRight: 15,
-    paddingLeft: 15,
-    backgroundColor: '#ffffff',
-  },
-  buttonAddImage: {
-    width: 13,
-    height: 15,
-    marginRight: 5,
-  },
-  buttonAddText: {
-    fontSize: 15,
-    fontWeight: 'bold',
+  infoText: {
+    fontSize: 14,
     color: CommonStyles.COLOR_PRIMARY,
   },
-  bulletMy: {
-    flexDirection: 'row',
+  buttonItem: {
+    width: 80,
+    height: 25,
+    marginTop: 28,
+    marginLeft: 10,
+    justifyContent: 'center',
     alignItems: 'center',
-    height: 30,
-    paddingRight: 10,
-    paddingLeft: 10,
-    marginRight: 10,
     borderWidth: 1,
-    borderColor: '#ffffff',
   },
-  bulletMyImage: {
-    width: 9,
-    height: 11,
-    marginRight: 5,
+  buttonItemDisabled: {
+    borderColor: '#C7C7C7',
   },
-  bulletMyText: {
-    fontSize: 15,
-    color: '#ffffff',
+  buttonItemActive: {
+    borderColor: CommonStyles.COLOR_PRIMARY,
+    backgroundColor: CommonStyles.COLOR_PRIMARY,
   },
-  buyText: {
+  buttonText: {
     fontSize: 13,
+    fontWeight: '400',
+  },
+  buttonTextDisabled: {
+    color: '#ACACAC',
+  },
+  buttonTextActive: {
     color: '#ffffff',
-  },
-  buyTextBullet: {
-    opacity: 0.2,
-    paddingRight: 5,
-    paddingLeft: 5,
-  },
-  finishText: {
-    paddingTop: 5,
-    fontSize: 11,
-    color: '#000000',
-  },
-  membershipDescText: {
-    fontSize: 13,
-    color: '#000000',
   },
 });
 
@@ -131,21 +78,22 @@ export default class VideoPaymentStatus extends React.Component {
 
   renderFreeContent() {
     return (
-      <View
-        style={[
-          CommonStyles.alignJustifyContentBetween,
-          styles.paymentContainer,
-        ]}
-      >
+      <View style={[CommonStyles.alignJustifyFlex, styles.contentContainer]}>
         <View>
-          <View style={[CommonStyles.alignJustifyFlex, styles.priceContainer]}>
-            <Text style={styles.priceOriginal}>₩0</Text>
+          <View style={{ height: 22 }} />
+          <View>
+            <Text style={styles.paymentText}>₩0</Text>
           </View>
         </View>
-
-        <View>
-          <View style={styles.buttonBuy}>
-            <Text style={styles.buttonBuyText}>무료</Text>
+        <View style={{ marginLeft: 'auto' }}>
+          <View style={CommonStyles.alignJustifyFlex}>
+            <TouchableOpacity activeOpacity={0.9}>
+              <View style={[styles.buttonItem, styles.buttonItemActive]}>
+                <Text style={[styles.buttonText, styles.buttonTextActive]}>
+                  무료
+                </Text>
+              </View>
+            </TouchableOpacity>
           </View>
         </View>
       </View>
@@ -154,28 +102,21 @@ export default class VideoPaymentStatus extends React.Component {
 
   renderUserHaveMembership() {
     return (
-      <View
-        style={[
-          CommonStyles.alignJustifyContentBetween,
-          styles.paymentContainer,
-        ]}
-      >
+      <View style={[CommonStyles.alignJustifyFlex, styles.contentContainer]}>
         <View>
-          <View style={[CommonStyles.alignJustifyFlex, styles.priceContainer]}>
-            <Text style={styles.membershipDescText}>무제한 시청</Text>
+          <View>
+            <Text style={styles.paymentText}>무제한 시청</Text>
           </View>
         </View>
-
-        <View>
-          <View
-            style={[
-              styles.buttonBuy,
-              { width: 'auto', alignSelf: 'flex-start', paddingHorizontal: 12 },
-            ]}
-          >
-            <Text style={styles.buttonBuyText}>
-              {globalStore.currentMembership.type_text}
-            </Text>
+        <View style={{ marginLeft: 'auto' }}>
+          <View style={CommonStyles.alignJustifyFlex}>
+            <TouchableOpacity activeOpacity={0.9}>
+              <View style={[styles.buttonItem, styles.buttonItemActive]}>
+                <Text style={[styles.buttonText, styles.buttonTextActive]}>
+                  {globalStore.currentMembership.type_text}
+                </Text>
+              </View>
+            </TouchableOpacity>
           </View>
         </View>
       </View>
@@ -184,20 +125,21 @@ export default class VideoPaymentStatus extends React.Component {
 
   renderUserOwned() {
     return (
-      <View
-        style={[
-          CommonStyles.alignJustifyContentBetween,
-          styles.paymentContainer,
-        ]}
-      >
+      <View style={[CommonStyles.alignJustifyFlex, styles.contentContainer]}>
         <View>
-          <View style={[CommonStyles.alignJustifyFlex, styles.priceContainer]}>
-            <Text style={styles.stateText}>영구소장</Text>
+          <View>
+            <Text style={styles.paymentText}>영구소장</Text>
           </View>
         </View>
-        <View>
-          <View style={styles.buttonBuy}>
-            <Text style={styles.buttonBuyText}>소장중</Text>
+        <View style={{ marginLeft: 'auto' }}>
+          <View style={CommonStyles.alignJustifyFlex}>
+            <TouchableOpacity activeOpacity={0.9}>
+              <View style={[styles.buttonItem, styles.buttonItemActive]}>
+                <Text style={[styles.buttonText, styles.buttonTextActive]}>
+                  소장중
+                </Text>
+              </View>
+            </TouchableOpacity>
           </View>
         </View>
       </View>
@@ -206,22 +148,26 @@ export default class VideoPaymentStatus extends React.Component {
 
   renderUserRent(expire_at) {
     return (
-      <View
-        style={[
-          CommonStyles.alignJustifyContentBetween,
-          styles.paymentContainer,
-        ]}
-      >
+      <View style={[CommonStyles.alignJustifyFlex, styles.contentContainer]}>
         <View>
-          <View style={[CommonStyles.alignJustifyFlex, styles.priceContainer]}>
-            <Text style={styles.stateText}>
-              {moment(expire_at).format('YYYY/MM/DD')} 만료
+          <View style={{ height: 22 }}>
+            <Text style={styles.labelText}>이용 가능 기간</Text>
+          </View>
+          <View>
+            <Text style={styles.paymentText}>
+              {moment(expire_at).format('YYYY년 MM월 DD일')}
             </Text>
           </View>
         </View>
-        <View>
-          <View style={styles.buttonBuy}>
-            <Text style={styles.buttonBuyText}>소장중</Text>
+        <View style={{ marginLeft: 'auto' }}>
+          <View style={CommonStyles.alignJustifyFlex}>
+            <TouchableOpacity activeOpacity={0.9}>
+              <View style={[styles.buttonItem, styles.buttonTextDisabled]}>
+                <Text style={[styles.buttonText, styles.buttonTextDisabled]}>
+                  소장중
+                </Text>
+              </View>
+            </TouchableOpacity>
           </View>
         </View>
       </View>
@@ -234,42 +180,37 @@ export default class VideoPaymentStatus extends React.Component {
     }
 
     return (
-      <View
-        style={[
-          CommonStyles.alignJustifyContentBetween,
-          styles.paymentContainer,
-        ]}
-      >
+      <View style={[CommonStyles.alignJustifyFlex, styles.contentContainer]}>
         <View>
-          <View style={[CommonStyles.alignJustifyFlex, styles.priceContainer]}>
-            <Text style={styles.priceOriginal}>
-              ₩{numeral(userPrice).format('0,0')}
-            </Text>
-
+          <View style={{ height: 22 }}>
             {origPrice !== userPrice
               ? (userPrice > 0 || userPrice !== origPrice) && (
-                  <Text style={styles.priceText}>
-                    <Text style={styles.priceDiscount}>
-                      ₩{numeral(origPrice).format('0,0')}
-                    </Text>
+                  <Text style={styles.originPriceText}>
+                    ₩{numeral(origPrice).format('0,0')}
                   </Text>
                 )
               : undefined}
-
-            <Text style={styles.priceText}>
-              <Text style={styles.rentDurationText}>
-                ({rentDuration}
-                일)
-              </Text>
+          </View>
+          <View>
+            <Text style={styles.paymentText}>
+              ₩{numeral(userPrice).format('0,0')}
+              <Text style={styles.durationText}>/{rentDuration}일</Text>
             </Text>
           </View>
         </View>
-        <View>
-          <TouchableOpacity onPress={this.props.addToCart}>
-            <View style={styles.buttonBuy}>
-              <Text style={styles.buttonBuyText}>구매</Text>
-            </View>
-          </TouchableOpacity>
+        <View style={{ marginLeft: 'auto' }}>
+          <View style={CommonStyles.alignJustifyFlex}>
+            <TouchableOpacity
+              activeOpacity={0.9}
+              onPress={this.props.addToCart}
+            >
+              <View style={[styles.buttonItem, styles.buttonItemActive]}>
+                <Text style={[styles.buttonText, styles.buttonTextActive]}>
+                  구매
+                </Text>
+              </View>
+            </TouchableOpacity>
+          </View>
         </View>
       </View>
     );
@@ -277,15 +218,8 @@ export default class VideoPaymentStatus extends React.Component {
 
   renderPermissionLoading() {
     return (
-      <View
-        style={[
-          CommonStyles.alignJustifyContentBetween,
-          styles.paymentContainer,
-        ]}
-      >
-        <Text style={[styles.priceText, { alignItems: 'center' }]}>
-          구매 정보를 불러오는 중입니다.
-        </Text>
+      <View style={[CommonStyles.alignJustifyFlex, styles.contentContainer]}>
+        <Text style={styles.paymentText}>구매 정보를 불러오는 중입니다.</Text>
       </View>
     );
   }
