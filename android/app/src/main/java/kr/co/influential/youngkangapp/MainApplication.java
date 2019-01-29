c71f09de271da9b1041a036bf357dcbeaba1d267
hyuk.woo <hyuk.woo@influential.co.kr>
2019-01-29 11:43:43 +0900

    react-native-sentry 테스트 등록 중
    Please open
    https://sentry.io/account/settings/wizard/5l03ctp0icw7k9zwxiz0rpcttbd0skhiyehvdhwpdg5uogwayywftsuzlu3qh69x/
    in your browser (if it's not open already)
    
    Please select your project in Sentry: Influential / welaaa-rn

--- android/app/src/main/java/kr/co/influential/youngkangapp/MainApplication.java
+++ android/app/src/main/java/kr/co/influential/youngkangapp/MainApplication.java
@@ -16,6 +16,7 @@ import com.crashlytics.android.Crashlytics;
 import com.crashlytics.android.core.CrashlyticsCore;
 import com.facebook.CallbackManager;
 import com.facebook.react.ReactApplication;
+import io.sentry.RNSentryPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.bridge.Arguments;
@@ -112,6 +113,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNSentryPackage(),
           new RNAppsFlyerPackage(MainApplication.this),
           new IAmPortPackage(),
           new VectorIconsPackage(),
