225adc984064e72eda7ab3f05f587d7ebeaa01d0
nanuRuya <uru@grotesq.com>
2019-02-01 19:44:38 +0900

    design-2.0-#89: 최근재생클래스 썸네일 사이즈 수정

--- src/scripts/components/home/ClassContinueList.js
+++ src/scripts/components/home/ClassContinueList.js
@@ -13,40 +13,17 @@ import IcPlay from '../../../images/ic-class-continue-play.png';
 import Native from '../../commons/native';
 import Carousel from 'react-native-snap-carousel';
 import _ from 'underscore';
+import ClassContinueListItem from './ClassContinueListItem';
 
 const styles = StyleSheet.create({
-  continueGrid: {
-    marginTop: 20,
+  classContainer: {},
+  slide: {
+    width: Dimensions.get('window').width * 0.55,
+    paddingRight: 10,
   },
-  continueItem: {
-    width: '48%',
-    borderWidth: 1,
-    borderColor: '#dddddd',
-  },
-  thumbnail: {
-    position: 'relative',
-    width: '100%',
-    paddingTop: '20%',
-    paddingBottom: '20%',
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#efefef',
-  },
-  thumbnailTitle: {
-    position: 'absolute',
-    top: 10,
-    left: '8%',
-    width: '84%',
-    fontSize: 14,
-    fontWeight: '800',
-    color: '#ffffff',
-  },
-  play: {
-    position: 'absolute',
-    left: '50%',
-    top: '45%',
-    width: 44,
-    height: 44,
+  slideInnerContainer: {
+    flex: 1,
+    width: Dimensions.get('window').width,
   },
 });
 
@@ -57,42 +34,28 @@ export default class ClassContinueList extends React.Component {
 
   _renderItem({ item }) {
     return (
-      <View borderRadius={10}>
-        <TouchableOpacity
-          activeOpacity={0.9}
-          onPress={() => Native.play(item.data.cid)}
-        >
-          <ImageBackground
-            source={{ uri: item.data.images ? item.data.images.wide : null }}
-            resizeMode="cover"
-            style={styles.thumbnail}
-            borderRadius={10}
-          >
-            <Text
-              style={styles.thumbnailTitle}
-              ellipsizeMode={'tail'}
-              numberOfLines={1}
-            >
-              {item.data.headline}
-            </Text>
-            <View style={styles.play}>
-              <Image source={IcPlay} style={CommonStyles.fullImg} />
-            </View>
-          </ImageBackground>
-        </TouchableOpacity>
+      <View style={styles.slide}>
+        <ClassContinueListItem
+          itemData={item}
+          style={styles.slideInnerContainer}
+        />
       </View>
     );
   }
 
   render() {
-    let windowWidth = Dimensions.get('window').width;
-    let itemWidth = 270;
+    let originData = [];
+    let itemData = [];
+    if (_.isObject(this.props.itemData)) {
+      originData = _.map(this.props.itemData, item => item);
+      itemData = originData.slice(0, 5);
+    }
 
-    let originData = _.map(this.props.itemData, item => item);
-    let itemData = originData.slice(0, 5);
+    let windowWidth = Dimensions.get('window').width;
+    let itemWidth = windowWidth * 0.55;
 
     return (
-      <View>
+      <View style={styles.classContainer}>
         {this.props.itemData && this.props.itemData.length > 0 && (
           <Carousel
             data={itemData}
@@ -102,7 +65,7 @@ export default class ClassContinueList extends React.Component {
             layout={'default'}
             activeSlideAlignment={'start'}
             inactiveSlideOpacity={1}
-            inactiveSlideScale={0.95}
+            inactiveSlideScale={1}
           />
         )}
       </View>
--- src/scripts/components/home/ClassContinueListItem.js
+++ src/scripts/components/home/ClassContinueListItem.js
@@ -1,1 +1,90 @@
+import React from 'react';
+import {
+  Text,
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  ImageBackground,
+  Image,
+  Dimensions,
+} from 'react-native';
+import CommonStyles from '../../../styles/common';
 import IcPlay from '../../../images/ic-play-class-continue.png';
+import Native from '../../commons/native';
+import Carousel from 'react-native-snap-carousel';
+import _ from 'underscore';
+
+const styles = StyleSheet.create({
+  continueGrid: {
+    marginTop: 20,
+  },
+  continueItem: {
+    width: '48%',
+    borderWidth: 1,
+    borderColor: '#dddddd',
+  },
+  thumbnail: {
+    position: 'relative',
+    width: '100%',
+    paddingTop: '20.416666666%',
+    paddingBottom: '20.416666666%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#efefef',
+  },
+  thumbnailTitle: {
+    position: 'absolute',
+    top: 10,
+    left: '8%',
+    width: '84%',
+    fontSize: 14,
+    fontWeight: '800',
+    color: '#ffffff',
+  },
+  play: {
+    position: 'absolute',
+    left: '50%',
+    top: '45%',
+    width: 40,
+    height: 40,
+    marginLeft: -20,
+  },
+});
+
+export default class ClassContinueListItem extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <View borderRadius={10}>
+        <TouchableOpacity
+          activeOpacity={0.9}
+          onPress={() => Native.play(this.props.itemData.data.cid)}
+        >
+          <ImageBackground
+            source={{
+              uri: this.props.itemData.data.images
+                ? this.props.itemData.data.images.wide
+                : null,
+            }}
+            resizeMode="cover"
+            style={styles.thumbnail}
+            borderRadius={10}
+          >
+            <Text
+              style={styles.thumbnailTitle}
+              ellipsizeMode={'tail'}
+              numberOfLines={1}
+            >
+              {this.props.itemData.data.headline}
+            </Text>
+            <View style={styles.play}>
+              <Image source={IcPlay} style={CommonStyles.fullImg} />
+            </View>
+          </ImageBackground>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
