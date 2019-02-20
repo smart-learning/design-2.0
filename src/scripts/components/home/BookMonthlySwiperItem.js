f2253f989bde9348546a3dbd8fc86f81cc2782b2
nanuRuya <uru@grotesq.com>
2019-02-20 18:02:19 +0900

    design-2.0-#198: 이달의 책 터치영역 변경

--- src/scripts/components/home/BookMonthlySwiperItem.js
+++ src/scripts/components/home/BookMonthlySwiperItem.js
@@ -1,10 +1,5 @@
 import React from 'react';
-import {
-  Text,
-  View,
-  StyleSheet,
-  TouchableOpacity,
-} from 'react-native';
+import { Text, View, StyleSheet, TouchableOpacity } from 'react-native';
 import { withNavigation } from 'react-navigation';
 import CommonStyles from '../../../styles/common';
 import { observer } from 'mobx-react';
@@ -118,50 +113,58 @@ class BookMonthlySwiperItem extends React.Component {
                 })
               }
             >
-              <FastImage
-                source={{ uri: itemData.book_a.audiobook.images.cover }}
-                style={styles.thumbnail}
-                resizeMode={FastImage.resizeMode.contain}
-              />
-            </TouchableOpacity>
-            <Text style={styles.title} ellipsizeMode={'tail'} numberOfLines={3}>
-              {itemData.book_a.title}
-            </Text>
-            <View style={styles.mentorInfo}>
-              <View style={CommonStyles.alignJustifyContentBetween}>
-                <View>
-                  <View style={styles.mentorLabel} borderRadius={8}>
-                    <Text style={styles.mentorLabelText}>리딩멘토</Text>
-                  </View>
-                  <Text
-                    style={styles.mentorName}
-                    ellipsizeMode={'tail'}
-                    numberOfLines={1}
-                  >
-                    {itemData.book_a.mentor.name
-                      ? itemData.book_a.mentor.name
-                      : null}
-                  </Text>
-                </View>
+              <View>
                 <FastImage
-                  source={{
-                    uri: itemData.book_a.mentor.images.profile
-                      ? itemData.book_a.mentor.images.profile
-                      : null,
-                  }}
-                  style={styles.mentorImage}
+                  source={{ uri: itemData.book_a.audiobook.images.cover }}
+                  style={styles.thumbnail}
                   resizeMode={FastImage.resizeMode.contain}
                 />
+                <Text
+                  style={styles.title}
+                  ellipsizeMode={'tail'}
+                  numberOfLines={3}
+                >
+                  {itemData.book_a.title}
+                </Text>
+                <View style={styles.mentorInfo}>
+                  <View style={CommonStyles.alignJustifyContentBetween}>
+                    <View>
+                      <View style={styles.mentorLabel} borderRadius={8}>
+                        <Text style={styles.mentorLabelText}>리딩멘토</Text>
+                      </View>
+                      <Text
+                        style={styles.mentorName}
+                        ellipsizeMode={'tail'}
+                        numberOfLines={1}
+                      >
+                        {itemData.book_a.mentor.name
+                          ? itemData.book_a.mentor.name
+                          : null}
+                      </Text>
+                    </View>
+                    <FastImage
+                      source={{
+                        uri: itemData.book_a.mentor.images.profile
+                          ? itemData.book_a.mentor.images.profile
+                          : null,
+                      }}
+                      style={styles.mentorImage}
+                      resizeMode={FastImage.resizeMode.contain}
+                    />
+                  </View>
+                </View>
+                <View style={styles.itemHr} />
+                <Text
+                  style={styles.mentorMemo}
+                  ellipsizeMode={'tail'}
+                  numberOfLines={2}
+                >
+                  {itemData.book_a.mentor.memo
+                    ? itemData.book_a.mentor.memo
+                    : null}
+                </Text>
               </View>
-            </View>
-            <View style={styles.itemHr} />
-            <Text
-              style={styles.mentorMemo}
-              ellipsizeMode={'tail'}
-              numberOfLines={2}
-            >
-              {itemData.book_a.mentor.memo ? itemData.book_a.mentor.memo : null}
-            </Text>
+            </TouchableOpacity>
           </View>
 
           <View style={styles.bookItem}>
@@ -174,50 +177,58 @@ class BookMonthlySwiperItem extends React.Component {
                 })
               }
             >
-              <FastImage
-                source={{ uri: itemData.book_b.audiobook.images.cover }}
-                style={styles.thumbnail}
-                resizeMode={FastImage.resizeMode.contain}
-              />
-            </TouchableOpacity>
-            <Text style={styles.title} ellipsizeMode={'tail'} numberOfLines={3}>
-              {itemData.book_b.title}
-            </Text>
-            <View style={styles.mentorInfo}>
-              <View style={CommonStyles.alignJustifyContentBetween}>
-                <View>
-                  <View style={styles.mentorLabel} borderRadius={8}>
-                    <Text style={styles.mentorLabelText}>리딩멘토</Text>
-                  </View>
-                  <Text
-                    style={styles.mentorName}
-                    ellipsizeMode={'tail'}
-                    numberOfLines={1}
-                  >
-                    {itemData.book_b.mentor.name
-                      ? itemData.book_b.mentor.name
-                      : null}
-                  </Text>
-                </View>
+              <View>
                 <FastImage
-                  source={{
-                    uri: itemData.book_b.mentor.images.profile
-                      ? itemData.book_b.mentor.images.profile
-                      : null,
-                  }}
-                  style={styles.mentorImage}
+                  source={{ uri: itemData.book_b.audiobook.images.cover }}
+                  style={styles.thumbnail}
                   resizeMode={FastImage.resizeMode.contain}
                 />
+                <Text
+                  style={styles.title}
+                  ellipsizeMode={'tail'}
+                  numberOfLines={3}
+                >
+                  {itemData.book_b.title}
+                </Text>
+                <View style={styles.mentorInfo}>
+                  <View style={CommonStyles.alignJustifyContentBetween}>
+                    <View>
+                      <View style={styles.mentorLabel} borderRadius={8}>
+                        <Text style={styles.mentorLabelText}>리딩멘토</Text>
+                      </View>
+                      <Text
+                        style={styles.mentorName}
+                        ellipsizeMode={'tail'}
+                        numberOfLines={1}
+                      >
+                        {itemData.book_b.mentor.name
+                          ? itemData.book_b.mentor.name
+                          : null}
+                      </Text>
+                    </View>
+                    <FastImage
+                      source={{
+                        uri: itemData.book_b.mentor.images.profile
+                          ? itemData.book_b.mentor.images.profile
+                          : null,
+                      }}
+                      style={styles.mentorImage}
+                      resizeMode={FastImage.resizeMode.contain}
+                    />
+                  </View>
+                </View>
+                <View style={styles.itemHr} />
+                <Text
+                  style={styles.mentorMemo}
+                  ellipsizeMode={'tail'}
+                  numberOfLines={2}
+                >
+                  {itemData.book_b.mentor.memo
+                    ? itemData.book_b.mentor.memo
+                    : null}
+                </Text>
               </View>
-            </View>
-            <View style={styles.itemHr} />
-            <Text
-              style={styles.mentorMemo}
-              ellipsizeMode={'tail'}
-              numberOfLines={2}
-            >
-              {itemData.book_b.mentor.memo ? itemData.book_b.mentor.memo : null}
-            </Text>
+            </TouchableOpacity>
           </View>
         </View>
       </View>
